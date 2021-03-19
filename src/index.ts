ac52ee49a874fdd741637958c7aff40579741a6c
Bertrandmc <bemacode@gmail.com>
2021-03-19 14:15:01 +0000

    Dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { server } from "./server";
 import { PORT, NODE_ENV } from "./config";
 import { logger } from "./services/logger";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 server.listen(PORT, () => {
   logger.info(`Server listening on PORT: ${PORT}, NODE_ENV: ${NODE_ENV}`);
