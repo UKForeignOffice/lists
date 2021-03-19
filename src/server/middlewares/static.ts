82827ccd68a57f49da75dd3eb9477ba57ac45879
Bertrandmc <bemacode@gmail.com>
2021-03-19 14:17:01 +0000

    Add accessible autocomplete and govukjs static assets paths

--- src/server/middlewares/static.ts
+++ src/server/middlewares/static.ts
@@ -1,10 +1,22 @@
 import path from "path";
 import express, { Express } from "express";
 
+const ROOT = process.cwd();
+const NODE_MODULES = path.join(ROOT, "node_modules");
+const GOVUK_FRONTEND = path.join(NODE_MODULES, "govuk-frontend", "govuk");
+
+const publicFolderPath = path.join(__dirname, "..", "..", "public");
+const govUKAssetsFolderPath = path.join(GOVUK_FRONTEND, "assets");
+const govUKAllJSPath = path.join(GOVUK_FRONTEND, "all.js");
+const accessibleAutoCompletePath = path.join(
+  NODE_MODULES,
+  "accessible-autocomplete",
+  "dist"
+);
+
 export const configureStaticServer = (server: Express): void => {
-  const publicFolderPath = path.join(__dirname, "..", "..", "public");
-  const govUKAssetsFolderPath = path.join(process.cwd(), "/node_modules/govuk-frontend/govuk/assets");
-  
   server.use("/assets", express.static(publicFolderPath));
   server.use("/assets", express.static(govUKAssetsFolderPath));
+  server.use("/assets/govuk-frontend/all.js", express.static(govUKAllJSPath));
+  server.use("/assets", express.static(accessibleAutoCompletePath));
 };
