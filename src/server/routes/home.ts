225d69e6ae27374bed7d6d7290bfec3998998aba
Bertrandmc <bemacode@gmail.com>
2021-03-19 14:19:23 +0000

    Adjust home page template

--- src/server/routes/home.ts
+++ src/server/routes/home.ts
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("home.html", {
+  res.render("home-page.html", {
     title: "Home Page",
   });
 });
