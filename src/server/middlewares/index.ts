64efa06961b405a29d6db17dcaa9be31dbddda36
Bertrandmc <bemacode@gmail.com>
2021-03-19 11:14:08 +0000

    Expose new middlewares

--- src/server/middlewares/index.ts
+++ src/server/middlewares/index.ts
@@ -2,3 +2,5 @@ export { default as helmet } from "./helmet";
 export { default as logger } from "./logger";
 export { default as compression } from "./compression";
 export { configureViews } from "./views";
+export { configureStaticServer } from "./static";
+export { configureErrorHandlers } from "./error-handlers"
