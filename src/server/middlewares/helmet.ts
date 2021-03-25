c67c46f0df6d87418c8ee3428b4c3e9b56ee0c4d
Bertrandmc <bemacode@gmail.com>
2021-03-25 17:17:50 +0000

    Disable helmet contentSecurityPolicy

--- src/server/middlewares/helmet.ts
+++ src/server/middlewares/helmet.ts
@@ -4,6 +4,5 @@ import helmet from "helmet";
 export default (): RequestHandler =>
   helmet({
     referrerPolicy: { policy: "no-referrer" },
-    contentSecurityPolicy:
-      process.env.NODE_ENV === "production" ? undefined : false,
+    contentSecurityPolicy: false, // TODO: this is breaking dev process.env.NODE_ENV === "production" ? undefined : false,
   });
