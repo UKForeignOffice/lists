8d3f0b165ba0c9af3a7121f6b1f974d902303a5a
Bertrandmc <bemacode@gmail.com>
2021-03-25 20:06:19 +0000

    set databse schema

--- src/server/models/database.ts
+++ src/server/models/database.ts
@@ -3,5 +3,5 @@ import { Pool } from "pg";
 import { DATABASE_URL } from "config";
 
 export const db = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString: (DATABASE_URL ?? "") + "?schema=public",
 });
