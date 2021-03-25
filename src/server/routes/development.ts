e31087c59486f52653da4964acacfa5b55b88c40
Bertrandmc <bemacode@gmail.com>
2021-03-25 19:42:36 +0000

    reset-db route

--- src/server/routes/development.ts
+++ src/server/routes/development.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { exec } from "child_process";
 import { prisma } from "server/models/prisma-client";
 import { populateDb } from "server/models/seed-data/populate-database";
 import { createGeoLocationTable } from "server/models/helpers";
@@ -27,6 +28,12 @@ router.get("/populate-db", (req, res) => {
     .catch((error) => {
       res.send({ error });
     });
+});
+
+router.get("/reset-db", (req, res) => {
+  exec("npm run prisma:reset", (error, stdout) => {
+    res.send({ error, stdout });
+  });
 });
 
 export default router;
