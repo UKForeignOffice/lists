064c25dc25e86ec861002e37bdf8e38b6f43c1f3
Bertrandmc <bemacode@gmail.com>
2021-03-26 09:35:52 +0000

    Add papertrail logger to debug dev environment

--- src/config/server-config.ts
+++ src/config/server-config.ts
@@ -5,7 +5,11 @@ dotenv.config();
 export const PORT = process.env.PORT ?? 3000;
 export const LOG_LEVEL = process.env.LOG_LEVEL ?? "info";
 export const NODE_ENV = process.env.NODE_ENV ?? "development";
+export const LOCAL_DEV = process.env.LOCAL_DEV === "true";
 export const DEBUG = process.env.DEBUG === "true";
-export const LOCATION_SERVICE_ACCESS_KEY = process.env.LOCATION_SERVICE_ACCESS_KEY
-export const LOCATION_SERVICE_SECRET_KEY = process.env.LOCATION_SERVICE_SECRET_KEY
-export const LOCATION_SERVICE_INDEX_NAME = process.env.LOCATION_SERVICE_INDEX_NAME
+export const LOCATION_SERVICE_ACCESS_KEY =
+  process.env.LOCATION_SERVICE_ACCESS_KEY;
+export const LOCATION_SERVICE_SECRET_KEY =
+  process.env.LOCATION_SERVICE_SECRET_KEY;
+export const LOCATION_SERVICE_INDEX_NAME =
+  process.env.LOCATION_SERVICE_INDEX_NAME;
--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,21 +1,33 @@
 import { createLogger, format, transports } from "winston";
-import { LOG_LEVEL } from "config";
+import { Papertrail } from "winston-papertrail";
+import { LOG_LEVEL, LOCAL_DEV } from "config";
+
+const transportsList = [
+  // - Write all logs with level `error` and below to `error.log`
+  // - Write all logs with level LOG_LEVEL and below to console.log
+  new transports.Console({
+    level: LOG_LEVEL,
+    format: format.combine(
+      format.timestamp(),
+      format.simple(),
+      format.colorize({ all: true })
+    ),
+  }),
+  new transports.File({ filename: "error.log", level: "error" }),
+];
+
+if (!LOCAL_DEV) {
+  transportsList.push(
+    new Papertrail({
+      host: "logs.papertrailapp.com",
+      port: 48692,
+    })
+  );
+}
 
 export const logger = createLogger({
   level: LOG_LEVEL,
   format: format.json(),
   defaultMeta: { service: "server" },
-  transports: [
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level LOG_LEVEL and below to console.log
-    new transports.Console({
-      level: LOG_LEVEL,
-      format: format.combine(
-        format.timestamp(),
-        format.simple(),
-        format.colorize({ all: true })
-      ),
-    }),
-    new transports.File({ filename: "error.log", level: "error" }),
-  ],
+  transports: transportsList,
 });
