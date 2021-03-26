74b5b2cda8adec3f06854bf51c502a5c2990750b
Bertrandmc <bemacode@gmail.com>
2021-03-26 12:57:50 +0000

    Logger ignore http get requests

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,2 @@
-import { logger } from "services/logger";
-
 export * from "./server-config";
 export * from "./database-config";
-
-logger.error("ENV", process.env);
--- src/services/logger.ts
+++ src/services/logger.ts
@@ -2,12 +2,20 @@ import { createLogger, format, transports } from "winston";
 import { Papertrail } from "winston-papertrail";
 import { LOG_LEVEL, LOCAL_DEV, isTest } from "config";
 
+const ignoreHttpGET = format((info) => {
+  if (info.message.includes("HTTP GET")) {
+    return false;
+  }
+  return info;
+});
+
 const transportsList = [
   // - Write all logs with level `error` and below to `error.log`
   // - Write all logs with level LOG_LEVEL and below to console.log
   new transports.Console({
     level: LOG_LEVEL,
     format: format.combine(
+      ignoreHttpGET(),
       format.timestamp(),
       format.simple(),
       format.colorize({ all: true })
@@ -31,3 +39,5 @@ export const logger = createLogger({
   defaultMeta: { service: "server" },
   transports: transportsList,
 });
+
+logger.error("ENV", process.env);
