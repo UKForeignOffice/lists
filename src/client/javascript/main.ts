d6de641933a39f75e442a4115e14662d4560c489
Bertrandmc <bemacode@gmail.com>
2021-04-12 14:50:49 +0100

    more webpack

--- src/client/javascript/main.ts
+++ src/client/javascript/main.ts
@@ -1,10 +1,3 @@
-import URLSearchParams from "url-search-params";
-
-
 (() => {
   // analytics
-  const {pathname} = window.location;
-  const urlParams = new URLSearchParams(window.location.search);
-
-  console.log({pathname, urlParams})
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -120,7 +120,7 @@ const server = {
   entry: path.resolve(__dirname, "src", "server", "index.ts"),
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "server.js",
+    // filename: "server.js",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".json"],
