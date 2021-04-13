82fa8f8a82e5ad81e8f104dfd6eb85e0b0adc49a
Bertrandmc <bemacode@gmail.com>
2021-04-13 12:09:59 +0100

    Comments

--- src/server/config/server-config.ts
+++ src/server/config/server-config.ts
@@ -8,6 +8,8 @@ export const LOG_LEVEL = process.env.LOG_LEVEL ?? "error";
 export const NODE_ENV = process.env.NODE_ENV ?? "development";
 export const LOCAL_DEV = process.env.LOCAL_DEV === "true";
 export const DEBUG = process.env.DEBUG === "true";
+
+// Location service
 export const LOCATION_SERVICE_ACCESS_KEY =
   process.env.LOCATION_SERVICE_ACCESS_KEY;
 export const LOCATION_SERVICE_SECRET_KEY =
@@ -15,10 +17,11 @@ export const LOCATION_SERVICE_SECRET_KEY =
 export const LOCATION_SERVICE_INDEX_NAME =
   process.env.LOCATION_SERVICE_INDEX_NAME;
 
-// TODO remove dev defaults
-export const GA_TRACKING_ID = process.env.GA_TRACKING_ID ?? "G-QMXES8PQGH";
-export const GA_API_SECRET = process.env.GA_API_SECRET ?? "F_jB5X0mT-6jERB9GmAYSg";
+// Google Analytics
+export const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
+export const GA_API_SECRET = process.env.GA_API_SECRET;
 
+// useful flags
 export const isProd = NODE_ENV === "production";
 export const isDev = NODE_ENV === "development";
 export const isTest = NODE_ENV === "test";
