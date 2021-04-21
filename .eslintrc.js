eeb39d259100d24a6f7dad5f328c44786dcbd5d6
Bertrandmc <bemacode@gmail.com>
2021-04-21 16:41:20 +0100

    Adjust eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,11 @@ module.exports = {
     "no-console": ["error"],
     "@typescript-eslint/restrict-template-expressions": 0,
   },
-  ignorePatterns: ["playground/", "dist/", ".eslintrc.js", "jest.config.js"],
+  ignorePatterns: [
+    "playground/",
+    "dist/",
+    ".eslintrc.js",
+    "jest.config.js",
+    "src/form-runner/form-runner-app/",
+  ],
 };
