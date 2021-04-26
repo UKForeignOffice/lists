bcb5f7c374f2f20edefe0b94b7c1b2531462a587
Bertrandmc <bemacode@gmail.com>
2021-04-26 18:44:39 +0100

    Disable papertrail

--- src/server/services/logger.ts
+++ src/server/services/logger.ts
@@ -1,7 +1,11 @@
 import { createLogger, format, transports } from "winston";
-import { PapertrailTransport } from "winston-papertrail-transport";
+// import { PapertrailTransport } from "winston-papertrail-transport";
 
-import { LOG_LEVEL, isLocalHost, isTest } from "server/config";
+import {
+  LOG_LEVEL,
+  // isLocalHost,
+  isTest,
+} from "server/config";
 
 const ignoreHttpGET = format((info) => {
   if (info.message.startsWith("HTTP GET")) {
@@ -37,16 +41,16 @@ const transportsList = [
 ];
 
 // Debug dev only
-if (!isLocalHost && !isTest) {
-  transportsList.push(
-    new PapertrailTransport({
-      level: LOG_LEVEL,
-      host: "logs.papertrailapp.com",
-      port: 48692,
-      format: formatters,
-    }) as any
-  );
-}
+// if (!isLocalHost && !isTest) {
+//   transportsList.push(
+//     new PapertrailTransport({
+//       level: LOG_LEVEL,
+//       host: "logs.papertrailapp.com",
+//       port: 48692,
+//       format: formatters,
+//     }) as any
+//   );
+// }
 
 export const logger = createLogger({
   level: LOG_LEVEL,
