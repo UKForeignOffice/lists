1d1d08c852e949d28e5e6b74a0e4a0da954f555d
Bertrandmc <bemacode@gmail.com>
2021-04-29 11:33:04 +0100

    Refactor

--- src/form-runner/install-form-runner.sh
+++ src/form-runner/install-form-runner.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 root_folder=$(pwd)
-form_runner_folder=./src/form-runner/form-runner-app
+form_runner_folder=./lib/form-runner
 form_runner_forms_folder="$form_runner_folder/runner/dist/server/forms"
+form_runner_env_file="$form_runner_folder/runner/.env"
 forms_json_folder=./src/form-runner/forms-json
 
 # rm -rf $form_runner_folder
@@ -22,24 +23,22 @@ else
   rm -rf ./designer
   rm -rf ./docs
   rm -rf ./smoke-tests
-  rm ./runner/src/server/forms/*
 
   echo "Form Runner Installed Successfully"
 fi
 
-# prepare .env
-rm -rf ./runner/.env
-echo "PORT=3001" >> ./runner/.env
-echo "PRIVACY_POLICY_URL=https://www.gov.uk/help/privacy-notice" >> ./runner/.env
-echo "SERVICE_NAME=Find a Professional Service Abroad" >> ./runner/.env
-echo "FEEDBACK_LINK=mailto:digitalservicesfeedback@fco.gov.uk" >> ./runner/.env
-echo "LOG_LEVEL=error" >> ./runner/.env
-echo "Form Runner .env Created Successfully"
-
 cd $root_folder
 
+# prepare .env
+rm $form_runner_env_file
+touch $form_runner_env_file
+echo "PORT=3001" >> $form_runner_env_file
+echo "PRIVACY_POLICY_URL=https://www.gov.uk/help/privacy-notice" >> $form_runner_env_file
+echo "FEEDBACK_LINK=mailto:digitalservicesfeedback@fco.gov.uk" >> $form_runner_env_file
+echo "LOG_LEVEL=error" >> $form_runner_env_file
+echo "Form Runner .env Created Successfully"
+
 # copy forms jsons
-rm "$form_runner_forms_folder/*" 
 cp -a "$forms_json_folder/." $form_runner_forms_folder
 echo "Forms JSON Files Copied Successfully"
  
