79f5cdded7a2c3712f5e8c0e0fa1356a870512eb
Bertrandmc <bemacode@gmail.com>
2021-05-05 17:48:19 +0100

    Disable dev routes outside localhost

--- src/server/routes/development.ts
+++ src/server/routes/development.ts
@@ -29,72 +29,74 @@ if (!isLocalHost) {
   router.get("/dev/*", devRateLimit);
 }
 
-router.get("/dev/inspect-db", (req, res) => {
-  describeDb()
-    .then((result) => res.json({ result }))
-    .catch((error) => res.json({ error }));
-});
-
-router.get("/dev/deploy-db", (req, res) => {
-  exec("npm run prisma:deploy", (error, stdout, stderr) => {
-    res.json({ error, stdout, stderr });
+if (isLocalHost) {
+  router.get("/dev/inspect-db", (req, res) => {
+    describeDb()
+      .then((result) => res.json({ result }))
+      .catch((error) => res.json({ error }));
   });
-});
 
-router.get("/dev/prepare-geo-db", (req, res) => {
-  createPostgis()
-    .then((resultPostgis) => {
-      createGeoLocationTable()
-        .then((resultsGeoTable) => {
-          res.json({ resultPostgis, resultsGeoTable });
-        })
-        .catch((error) => res.json({ error }));
-    })
-    .catch((error) => res.json({ error }));
-});
-
-router.get("/dev/reset-db", (req, res) => {
-  exec("npm run prisma:reset", (error, stdout) => {
-    res.json({ error, stdout });
+  router.get("/dev/deploy-db", (req, res) => {
+    exec("npm run prisma:deploy", (error, stdout, stderr) => {
+      res.json({ error, stdout, stderr });
+    });
   });
-});
 
-router.get("/dev/seed-db", (req, res) => {
-  seedDb(prisma)
-    .then((result) => {
-      res.json({ result });
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
-});
-
-router.get("/dev/dump-db", (req, res) => {
-  dumpDb()
-    .then((result) => {
-      res.json({ result });
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
-});
-
-router.get("/dev/list-applied-migrations", (req, res) => {
-  listAppliedMigrations()
-    .then((result) => {
-      res.json({ result });
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
-});
-
-router.get("/dev/debug-env", (req, res) => {
-  res.json({
-    LOCATION_SERVICE_ACCESS_KEY: LOCATION_SERVICE_ACCESS_KEY?.length,
-    LOCATION_SERVICE_SECRET_KEY: LOCATION_SERVICE_SECRET_KEY?.length,
-    LOCATION_SERVICE_INDEX_NAME: LOCATION_SERVICE_INDEX_NAME?.length,
+  router.get("/dev/prepare-geo-db", (req, res) => {
+    createPostgis()
+      .then((resultPostgis) => {
+        createGeoLocationTable()
+          .then((resultsGeoTable) => {
+            res.json({ resultPostgis, resultsGeoTable });
+          })
+          .catch((error) => res.json({ error }));
+      })
+      .catch((error) => res.json({ error }));
   });
-});
+
+  router.get("/dev/reset-db", (req, res) => {
+    exec("npm run prisma:reset", (error, stdout) => {
+      res.json({ error, stdout });
+    });
+  });
+
+  router.get("/dev/seed-db", (req, res) => {
+    seedDb(prisma)
+      .then((result) => {
+        res.json({ result });
+      })
+      .catch((error) => {
+        res.json({ error });
+      });
+  });
+
+  router.get("/dev/dump-db", (req, res) => {
+    dumpDb()
+      .then((result) => {
+        res.json({ result });
+      })
+      .catch((error) => {
+        res.json({ error });
+      });
+  });
+
+  router.get("/dev/list-applied-migrations", (req, res) => {
+    listAppliedMigrations()
+      .then((result) => {
+        res.json({ result });
+      })
+      .catch((error) => {
+        res.json({ error });
+      });
+  });
+
+  router.get("/dev/debug-env", (req, res) => {
+    res.json({
+      LOCATION_SERVICE_ACCESS_KEY: LOCATION_SERVICE_ACCESS_KEY?.length,
+      LOCATION_SERVICE_SECRET_KEY: LOCATION_SERVICE_SECRET_KEY?.length,
+      LOCATION_SERVICE_INDEX_NAME: LOCATION_SERVICE_INDEX_NAME?.length,
+    });
+  });
+}
 
 export default router;
