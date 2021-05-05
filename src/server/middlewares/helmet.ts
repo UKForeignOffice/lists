01deac0f5a30438a8d6289377010bcc734d1f2d7
Bertrandmc <bemacode@gmail.com>
2021-05-05 15:02:25 +0100

    Adjust helmet contentSecurityPolicy

--- src/server/middlewares/helmet.ts
+++ src/server/middlewares/helmet.ts
@@ -1,12 +1,12 @@
 import { Express } from "express";
 import helmet from "helmet";
+import { isLocalHost } from "server/config";
 
 export function configureHelmet(server: Express): void {
   server.use(
     helmet({
       referrerPolicy: { policy: "no-referrer" },
-      // TODO: this is breaking dev process.env.NODE_ENV === "production" ? undefined : false,
-      contentSecurityPolicy: false,
+      contentSecurityPolicy: !isLocalHost,
     })
   );
 }
