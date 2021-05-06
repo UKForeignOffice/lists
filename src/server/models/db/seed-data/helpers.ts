de569f825250a21ea87741fc18b65b9df89567d6
Bertrandmc <bemacode@gmail.com>
2021-05-06 13:41:51 +0100

    Refactor

--- src/server/models/db/seed-data/helpers.ts
+++ src/server/models/db/seed-data/helpers.ts
@@ -1,35 +1,14 @@
-import { PrismaClient, Country } from "@prisma/client";
-import { upperFirst } from "lodash";
+import { PrismaClient } from "@prisma/client";
 import { logger } from "server/services/logger";
 import { CountriesWithData } from "../../types";
 import { createLawyer, approveLawyer, publishLawyer } from "../../lawyers";
 import { LawyersFormWebhookData } from "server/services/form-runner";
 
-const postCodeExtractRegex: {
-  [propName: string]: RegExp;
-} = {
-  Thailand: /(\d{5})(?!.*\1)/gm,
-  France: /(\d{5})(?!.*\1)/gm,
-  Italy: /(\d{5})(?!.*\1)/gm,
-  Spain: /(\d{5})(?!.*\1)/gm,
-};
-
-function createLawyersQueryObjects(
-  country: Country,
+function createLawyerJson(
+  countryName: string,
   lawyers: any[]
 ): LawyersFormWebhookData[] {
-  const postCodeRegex = postCodeExtractRegex[country.name];
-
-  if (postCodeRegex === undefined) {
-    throw new Error(
-      `Missing postcode extract regex for country ${country.name}`
-    );
-  }
-
   return lawyers.map((lawyer) => {
-    const postCodeFromAddress: string =
-      lawyer.address?.match(postCodeRegex)?.["0"] ?? "";
-
     return {
       speakEnglish: true,
       englishSpeakLead: true,
@@ -46,9 +25,9 @@ function createLawyersQueryObjects(
       phoneNumber: lawyer.telephone,
       addressLine1: lawyer.address,
       addressLine2: undefined,
-      city: "",
-      postcode: postCodeFromAddress,
-      country: `${country.name}`,
+      city: lawyer.city ?? "",
+      postcode: lawyer.postcode ?? "",
+      country: `${countryName}`,
       areasOfLaw: lawyer.legalPracticeAreas?.replace(";", ","),
       canProvideLegalAid: lawyer.legalAid === "Yes",
       canOfferProBono: lawyer.proBonoService === "Yes",
@@ -84,35 +63,13 @@ export const populateCountryLawyers: PopulateCountryLawyers = async (
   lawyers,
   prisma
 ) => {
-  const name = upperFirst(countryName);
-  let country: Country;
-
-  try {
-    country = await prisma.country.upsert({
-      where: { name },
-      update: { name },
-      create: { name },
-    });
-  } catch (error) {
-    logger.error("Create country error", error);
-    return { country: countryName, error: error.message };
-  }
-
-  let lawyersInsetObjList;
-
-  try {
-    lawyersInsetObjList = createLawyersQueryObjects(country, lawyers);
-  } catch (error) {
-    logger.error("createLawyersQueryObjects", error);
-    return { country: countryName, error: error.message };
-  }
+  const lawyerJsonList = createLawyerJson(countryName, lawyers);
 
   let itemsInserted = 0;
-
   const errors: string[] = [];
 
-  for (let i = 0; i < lawyersInsetObjList.length; i++) {
-    const lawyer = lawyersInsetObjList[i];
+  for (let i = 0; i < lawyerJsonList.length; i++) {
+    const lawyer = lawyerJsonList[i];
 
     try {
       const newLawyer = await createLawyer(lawyer);
@@ -125,7 +82,7 @@ export const populateCountryLawyers: PopulateCountryLawyers = async (
   }
 
   const result = {
-    country: country.name,
+    country: countryName,
     totalSuccess: itemsInserted,
     errors,
   };
