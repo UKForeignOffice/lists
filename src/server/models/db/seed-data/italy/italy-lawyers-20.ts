8fafed4509562fe5978e33ec73b89043f14740d1
Bertrandmc <bemacode@gmail.com>
2021-05-06 13:03:59 +0100

    Adjust seed data address

--- src/server/models/db/seed-data/italy/italy-lawyers-20.ts
+++ src/server/models/db/seed-data/italy/italy-lawyers-20.ts
@@ -5,7 +5,9 @@ export default [
     email: "achille.campagna@avvocatocampagna.com",
     website: "http://www.avvocatocampagna.com",
     telephone: "+39 338 2026150",
-    address: "Via Cantù, 104 - 47891 San Marino",
+    address: "Via Cantù, 104",
+    postcode: "47891",
+    city: "San Marino",
     legalPracticeAreas:
       "Corporate; Criminal; Family; Health; International; Personal Injury; Real Estate; Tax",
     legalAid: "Yes",
--- src/server/models/db/seed-data/spain/spain-lawyers-2.ts
+++ src/server/models/db/seed-data/spain/spain-lawyers-2.ts
@@ -5,7 +5,9 @@ export default [
     website: "http://ardinessampedro.com/",
     telephone: "(+34) 922 751 196 & (+34) 658 828 687",
     address:
-      "Avenida de Los Playeros, nº 25 - 1º Derecha. 38650-Los Cristianos, Arona. Tenerife",
+      "Avenida de Los Playeros, nº 25 - 1º Derecha. Los Cristianos, Arona",
+    postcode: "38650",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas: "Family, criminal, civil & real estate",
     legalAid: "No",
@@ -16,8 +18,9 @@ export default [
     lawFirmName: "Cabrera y Ravina Abogados",
     website: "http://www.cabrerayravina.com",
     telephone: "(+34) 922 243 890",
-    address:
-      "Calle General Gutiérrez, nº 4 - 1º. 38002-Santa Cruz de Tenerife. Tenerife",
+    address: "Calle General Gutiérrez, nº 4 - 1º. Santa Cruz de Tenerife",
+    postcode: "38002",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Banking & finance, constructions & engineering, corporate, criminal, family & divorce, property",
@@ -30,7 +33,9 @@ export default [
     website: "http://www.decottalaw.com",
     telephone: "(+34) 922 719 520",
     address:
-      "Avenida de España, s/n. Centro Comercial Terranova, Locales 312-313, San Eugenio. 38660-Costa Adeje. Tenerife",
+      "Avenida de España, s/n. Centro Comercial Terranova, Locales 312-313, San Eugenio. Costa Adeje",
+    postcode: "38660",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Conveyancing, Inheritance & tax, commericial, civil, criminal, family law, wills & probate",
@@ -42,7 +47,9 @@ export default [
     lawFirmName: "Hodgson Abogados",
     website: "http://www.hodgson.es",
     telephone: "(+34) 922 247 365",
-    address: "Calle Pilar, nº 5 - 2º. 38002-Santa Cruz de Tenerife. Tenerife",
+    address: "Calle Pilar, nº 5 - 2º. Santa Cruz de Tenerife",
+    postcode: "38002",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Corporate, mercantile, maritime, civil, property, family, personal injury defence",
@@ -52,8 +59,9 @@ export default [
   {
     contactName: "Humberto Conçalves Abogado",
     telephone: "(+34) 653 679 154",
-    address:
-      "Calle Aguere, nº 15, Local Bajo-B. 38005-Santa Cruz de Tenerife. Tenerife",
+    address: "Calle Aguere, nº 15, Local Bajo-B. -Santa Cruz de Tenerife",
+    postcode: "38005",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Inheritance, wills, personal injury, family, Property & real estate, immigration, banking & finance, litigation & dispute resolution, corporate, commercial and employment law",
@@ -66,7 +74,9 @@ export default [
     website: "http://www.lawtenlawyers.com",
     telephone: "(+34) 735 937 & (+34) 652 293 325",
     address:
-      "Avenida Claudio Delgado Díaz, nº 99. Oficina 1ºB. Polígono Las Chafiras. 38639-San Miguel de Abona. Tenerife",
+      "Avenida Claudio Delgado Díaz, nº 99. Oficina 1ºB. Polígono Las Chafiras. San Miguel de Abona",
+    postcode: "38639",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Family, criminal, extradition, international, Property, will & affidavits",
@@ -79,7 +89,9 @@ export default [
     website: "http://www.avvo.es",
     telephone: "(+34) 922 751 524",
     address:
-      "Boulevard Chajofe, nº 3, Edificio Edén, Local 109. 38650-Los Cristianos, Arona. Tenerife",
+      "Boulevard Chajofe, nº 3, Edificio Edén, Local 109. Los Cristianos, Arona",
+    postcode: "38650",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas: "Family, criminal, civil, taxation & property",
     legalAid: "No",
@@ -90,7 +102,9 @@ export default [
     lawFirmName: "Website under construction",
     telephone: "(+34) 822 143 647 & (+34) 605 879 124",
     address:
-      "Avenida de Moscú, nº 38. Centro Comercial Aquamall, local 10. 38679-Costa Adeje. Tenerife",
+      "Avenida de Moscú, nº 38. Centro Comercial Aquamall, local 10. -Costa Adeje",
+    postcode: "38679",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Civil, mercantile, criminal, family & divorce, personal injury, timeshare, real estate",
@@ -103,7 +117,9 @@ export default [
     website: "http://www.tenerifesolicitors.com",
     telephone: "(+34) 922 717 845 & 0871 218 0063",
     address:
-      "Calle Colón, Urbanización Club Atlantis, Local 1. San Eugenio. 38660-Costa Adeje. Tenerife",
+      "Calle Colón, Urbanización Club Atlantis, Local 1. San Eugenio. -Costa Adeje",
+    postcode: "38660",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province, England & Wales",
     legalPracticeAreas:
       "Property, wills and probate, divorce, family, litigation and dispute resolution, business, timeshare",
@@ -116,7 +132,9 @@ export default [
     website: "http://www.vercelloneyasociados.es",
     telephone: "(+34) 922 717 202",
     address:
-      "Avenida Los Pueblos, nº 20. Centro Comercial San Eugenio, Local 72. 38660-Costa Adeje. Tenerife",
+      "Avenida Los Pueblos, nº 20. Centro Comercial San Eugenio, Local 72. -Costa Adeje",
+    postcode: "38660",
+    city: "Tenerife",
     regionsServed: "Santa Cruz de Tenerife province",
     legalPracticeAreas:
       "Company commercial, conveyancing, inheritance, tax, family",
@@ -128,8 +146,9 @@ export default [
     lawFirmName: "Wildpret Abogados",
     website: "http://www.wildpret.es",
     telephone: "(+34) 922 718 020",
-    address:
-      "Centro Comercial San Eugenio, local 97. 38670-Costa Adeje. Tenerife",
+    address: "Centro Comercial San Eugenio, local 97. -Costa Adeje",
+    postcode: "38670",
+    city: "Tenerife",
     regionsServed: "Canary Islands",
     legalPracticeAreas:
       "Conveyance, litigations, inheritance, wills, family, investments, civil, contracts, taxes",
