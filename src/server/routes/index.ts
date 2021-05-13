56f9ff4d1a672f67b67192c7bb072b889d8c19af
Bertrandmc <bemacode@gmail.com>
2021-05-13 17:19:45 +0100

    include auth and dashboard routes

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -3,10 +3,14 @@ import healthCheckRouter from "./health-check";
 import listsFinderRouter from "./lists";
 import developmentRouter from "./development";
 import sitemapRouter from "./sitemap";
+import authRouter from "./auth";
+import dashboardRouter from "./dashboard";
 
 export const configureRouter = (server: Express): void => {
   server.use(healthCheckRouter);
   server.use(listsFinderRouter);
   server.use(sitemapRouter);
   server.use(developmentRouter);
+  server.use(authRouter);
+  server.use(dashboardRouter);
 };
