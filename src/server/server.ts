cc271351582b4a6072124e403bd47e1655ab04ed
Bertrandmc <bemacode@gmail.com>
2021-05-13 17:21:23 +0100

    Configure server session and auth

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import {
+  configureViews,
   configureHelmet,
   configureLogger,
-  configureCompression,
-  configureViews,
   configureBodyParser,
+  configureCompression,
   configureStaticServer,
   configureErrorHandlers,
+  configureExpressSession,
   configureFormRunnerProxy,
 } from "./middlewares";
+import { configureAuth } from "./services/auth";
 import { configureRouter } from "./routes";
 
 export const server = express();
@@ -17,10 +19,14 @@ export const server = express();
 configureHelmet(server);
 configureLogger(server);
 configureCompression(server);
-// form runner proxy must be initialized before body parser
-configureFormRunnerProxy(server);
+configureFormRunnerProxy(
+  // form runner proxy must be initialized before body parser
+  server
+);
 configureBodyParser(server);
+configureExpressSession(server);
 configureViews(server);
+configureAuth(server);
 configureRouter(server);
 configureStaticServer(server);
 configureErrorHandlers(server);
