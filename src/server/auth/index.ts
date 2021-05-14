e8c98ef1e54067366342bb0d0fe7213cac8fb7cc
Bertrandmc <bemacode@gmail.com>
2021-05-14 13:16:00 +0100

    Cleanup auth exposed API

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -1,7 +1,3 @@
 export { User } from "./types";
 export { authRoutes } from "./constants";
 export { configureAuth, ensureAuthenticated } from "./helpers";
-export {
-  createAuthenticationJWT,
-  createAuthenticationPath,
-} from "./json-web-token";
