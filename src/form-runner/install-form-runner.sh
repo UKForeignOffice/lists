b639a6718fff9181cc085e592be24cf60736274e
Bertrandmc <bemacode@gmail.com>
2021-06-02 09:15:19 +0100

    Adjust form-runner .env

--- src/form-runner/install-form-runner.sh
+++ src/form-runner/install-form-runner.sh
@@ -6,8 +6,6 @@ form_runner_forms_folder="$form_runner_folder/runner/dist/server/forms"
 form_runner_env_file="$form_runner_folder/runner/.env"
 forms_json_folder=./src/form-runner/forms-json
 
-# rm -rf $form_runner_folder
-
 if [ -n "$(ls -A "$form_runner_folder/node_modules/.bin" 2>/dev/null)" ]
 then
   echo "Form Runner Already Installed"
@@ -36,6 +34,13 @@ echo "PORT=3001" >> $form_runner_env_file
 echo "PRIVACY_POLICY_URL=https://www.gov.uk/help/privacy-notice" >> $form_runner_env_file
 echo "FEEDBACK_LINK=mailto:digitalservicesfeedback@fco.gov.uk" >> $form_runner_env_file
 echo "LOG_LEVEL=error" >> $form_runner_env_file
+
+if test -f ".env"; then
+  sed -n '/^REDIS_HOST/p' .env >> $form_runner_env_file
+  sed -n '/^REDIS_PORT/p' .env >> $form_runner_env_file
+  sed -n '/^sandbox/p' .env >> $form_runner_env_file
+fi
+
 echo "Form Runner .env Created Successfully"
 
 # copy forms jsons
