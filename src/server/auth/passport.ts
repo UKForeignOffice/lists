72bec0500102cf5463fc56dbc19ded4604a6af18
Bertrandmc <bemacode@gmail.com>
2021-06-15 10:11:43 +0100

    Debug JwtStrategy

--- src/server/auth/passport.ts
+++ src/server/auth/passport.ts
@@ -11,6 +11,7 @@ import { findUserByEmail, createUser } from "server/models/user";
 import { AuthenticatedUser } from "./authenticated-user";
 
 import { JWT_ALGORITHM } from "./constants";
+import { logger } from "server/services/logger";
 
 export async function configurePassport(server: Express): Promise<void> {
   const JWT_SECRET = await getJwtSecret();
@@ -26,20 +27,26 @@ export async function configurePassport(server: Express): Promise<void> {
     new JwtStrategy(OPTIONS, async (token, done) => {
       const { user } = token;
 
+      logger.info(`JwtStrategy will try to find user ${JSON.stringify(user)}`);
+
       if (user.email !== undefined) {
         let userData = await findUserByEmail(user.email);
+        logger.info(`JwtStrategy findUserByEmail ${JSON.stringify(userData)}`);
 
         if (userData === undefined) {
+          logger.info(`JwtStrategy will create user ${JSON.stringify(user)}`);
           userData = await createUser({
             email: user.email,
             jsonData: {
               roles: [],
             },
           });
+          logger.info(`JwtStrategy created user ${JSON.stringify(userData)}`);
         }
 
         done(null, userData);
       } else {
+        logger.info(`JwtStrategy failed to login user ${JSON.stringify(user)}`);
         done(null, false);
       }
     })
