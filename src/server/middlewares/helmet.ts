9292129cafb6501ffa74765a138fb38ded8cfcb9
Bertrandmc <bemacode@gmail.com>
2021-06-15 13:03:11 +0100

    Temporary disable helmet contentSecurityPolicy setting

--- src/server/middlewares/helmet.ts
+++ src/server/middlewares/helmet.ts
@@ -1,11 +1,11 @@
-import { Express } from "express";
+import { Express, RequestHandler } from "express";
 import helmet from "helmet";
 
 export function configureHelmet(server: Express): void {
   server.use(
     helmet({
       referrerPolicy: { policy: "no-referrer" },
-      contentSecurityPolicy: false, // TODO: When true this breaks some scripts e.b autocomplete input
-    })
+      // contentSecurityPolicy: false, // TODO: When true this breaks some scripts e.b autocomplete input
+    }) as RequestHandler
   );
 }
