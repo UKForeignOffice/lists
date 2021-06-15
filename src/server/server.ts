53abf4225a605476a8731b52db634510e5f67607
Bertrandmc <bemacode@gmail.com>
2021-06-15 11:13:21 +0100

    Enable express trust proxy

--- src/server/server.ts
+++ src/server/server.ts
@@ -13,8 +13,13 @@ import {
 } from "./middlewares";
 import { configureAuth } from "./auth";
 import { configureRouter } from "./routes";
+import { isLocalHost, isTest } from "server/config";
 
 const server = express();
+
+if (!isLocalHost && !isTest) {
+  server.set("trust proxy", ["loopback", "linklocal", "uniquelocal"]);
+}
 
 export async function getServer(): Promise<Express> {
   configureHelmet(server);
