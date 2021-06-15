4f4cc19fc34f593e776e2b49c8adf6d21b482a50
Bertrandmc <bemacode@gmail.com>
2021-06-15 14:28:54 +0100

    Move development routes under dashboard

--- src/server/routes/development.ts
+++ src/server/routes/development.ts
@@ -1,24 +1,15 @@
 import express from "express";
 import { exec } from "child_process";
-import rateLimit from "express-rate-limit";
 import { listAppliedMigrations } from "server/models/helpers";
 import { populateDb } from "server/models/db/helpers";
-import { isLocalHost, GOVUK_NOTIFY_API_KEY } from "server/config";
+import { GOVUK_NOTIFY_API_KEY } from "server/config";
 import { createUser } from "server/models/user";
 import { UserRoles } from "server/models/types";
+import { dashboardRoutes } from "server/controllers/dashboard";
 
 const router = express.Router();
 
-const devRateLimit = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
-  max: 10,
-});
-
-if (!isLocalHost) {
-  router.get("/dev/*", devRateLimit);
-}
-
-router.get("/dev/reset-db", (req, res) => {
+router.get(`${dashboardRoutes.start}/dev/reset-db`, (req, res) => {
   req.setTimeout(5 * 60 * 1000);
 
   exec("npm run prisma:reset", () => {
@@ -32,17 +23,20 @@ router.get("/dev/reset-db", (req, res) => {
   });
 });
 
-router.get("/dev/list-applied-migrations", (req, res) => {
-  listAppliedMigrations()
-    .then((result) => {
-      res.json({ result });
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
-});
+router.get(
+  `${dashboardRoutes.start}/dev/list-applied-migrations`,
+  (req, res) => {
+    listAppliedMigrations()
+      .then((result) => {
+        res.json({ result });
+      })
+      .catch((error) => {
+        res.json({ error });
+      });
+  }
+);
 
-router.get("/dev/list-env-names", (req, res) => {
+router.get(`${dashboardRoutes.start}/dev/list-env-names`, (req, res) => {
   function isUpperCase(str: string): boolean {
     return str === str.toUpperCase();
   }
@@ -51,7 +45,7 @@ router.get("/dev/list-env-names", (req, res) => {
   res.json({ keys });
 });
 
-router.get("/dev/create-super-admin", (req, res) => {
+router.get(`${dashboardRoutes.start}/dev/create-super-admin`, (req, res) => {
   const { email, key } = req.query;
 
   if (
