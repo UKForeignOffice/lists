11f97ee755441bb51d17c880465bd09c0a073e91
Bertrandmc <bemacode@gmail.com>
2021-06-23 15:53:57 +0100

    Audit db schema and migration

---
+++ src/server/models/db/migrations/20210623144910_audit_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Audit" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "type" TEXT,
+    "jsonData" JSONB NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Audit.type_index" ON "Audit"("type");
+
+-- CreateIndex
+CREATE INDEX "Audit.jsonData_gin" ON "Audit" USING gin ("jsonData");
