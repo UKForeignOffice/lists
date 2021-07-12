8fd85f684c78d33d45d9ca08d7ddd89061a3fd28
Bertrandmc <bemacode@gmail.com>
2021-07-12 10:26:59 +0100

    Upgrade @types dependencies

--- src/server/middlewares/body-parser.ts
+++ src/server/middlewares/body-parser.ts
@@ -2,6 +2,5 @@ import { Express } from "express";
 import { json, urlencoded } from "body-parser";
 
 export function configureBodyParser(server: Express): void {
-  // @ts-expect-error
   server.use([json(), urlencoded({ extended: true })]);
 }
