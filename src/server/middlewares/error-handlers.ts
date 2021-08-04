7247b8c1b7ba28dc1efad8755e7868c182c5d8b8
Bertrandmc <bemacode@gmail.com>
2021-08-04 16:26:37 +0100

    Fix lint error

--- src/server/middlewares/error-handlers.ts
+++ src/server/middlewares/error-handlers.ts
@@ -1,4 +1,4 @@
-import { Express, Request, Response, NextFunction } from "express";
+import { Express, Request, Response } from "express";
 import { logger } from "server/services/logger";
 
 interface HttpException extends Error {
