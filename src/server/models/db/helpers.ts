b4331e590b0bec84df8124de804e4ec83225294b
Bertrandmc <bemacode@gmail.com>
2021-08-06 14:19:36 +0100

    Deprecate describeDb helper

--- src/server/models/db/helpers.ts
+++ src/server/models/db/helpers.ts
@@ -1,23 +1,6 @@
 import { prisma } from "./prisma-client";
-import { getDbPool } from "./database";
 import { logger } from "server/services/logger";
 import { seedDb } from "./seed-data/seed-db";
-
-export const describeDb = async (): Promise<any> => {
-  const query = `
-    SELECT table_schema,table_name FROM information_schema.tables
-    ORDER BY table_schema,table_name;
-  `;
-
-  try {
-    const db = getDbPool();
-    const result = await db.query(query);
-    return result;
-  } catch (error) {
-    logger.error("describeDb error:", error);
-    return error;
-  }
-};
 
 export async function populateDb(): Promise<string[]> {
   try {
