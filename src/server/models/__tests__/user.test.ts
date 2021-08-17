c9385f071d83fb1a62da834ed966f6aa2ee23f66
Bertrandmc <bemacode@gmail.com>
2021-08-17 13:23:59 +0100

    Dashboard user email case and audit model index (#246)
    * Enforce stored user email addresses are lower case
    
    * Adjust audit model jsonData index
    
    * Test email is lower case

--- src/server/models/__tests__/user.test.ts
+++ src/server/models/__tests__/user.test.ts
@@ -1,4 +1,4 @@
-import { pick } from "lodash";
+import { omit, pick } from "lodash";
 import { prisma } from "../db/__mocks__/prisma-client";
 import { UserRoles } from "../types";
 import {
@@ -67,12 +67,17 @@ describe("User Model:", () => {
 
     test("create command is correct", async () => {
       prisma.user.create.mockResolvedValue(sampleUser);
-      const newUser = pick(sampleUser, ["email", "jsonData"]);
-
-      await createUser(newUser);
+      
+      await createUser({
+        email: sampleUser.email.toUpperCase(),
+        jsonData: sampleUser.jsonData,
+      });
 
       expect(prisma.user.create).toHaveBeenCalledWith({
-        data: newUser,
+        data: {
+          email: sampleUser.email.toLowerCase(),
+          jsonData: sampleUser.jsonData,
+        },
       });
     });
 
@@ -103,14 +108,14 @@ describe("User Model:", () => {
         jsonData: { ...sampleUser.jsonData },
       };
 
-      const result = await updateUser(user.email, user);
+      const result = await updateUser(sampleUser.email.toUpperCase(), user);
 
       expect(result).toBe(sampleUser);
       expect(prisma.user.update).toHaveBeenCalledWith({
-        where: { email: user.email },
-        data: {
+        where: { email: sampleUser.email },
+        data: omit({
           ...user,
-        },
+        }, ["email"]),
       });
     });
 
