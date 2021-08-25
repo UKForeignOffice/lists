9e9ca1faa1a4ce3ff12367c0fe429d59307b85d2
Bertrandmc <bemacode@gmail.com>
2021-08-25 18:29:44 +0100

    Upgrade form runner dependency

--- src/server/components/formRunner/install-form-runner.sh
+++ src/server/components/formRunner/install-form-runner.sh
@@ -13,7 +13,7 @@ then
   echo "Form Runner Already Installed"
 else
   echo "Installing Form Runner"
-  git clone --depth 1 --branch 3.9.10-rc.784 https://github.com/XGovFormBuilder/digital-form-builder.git $form_runner_folder
+  git clone --depth 1 --branch 3.10.1-rc.789 https://github.com/XGovFormBuilder/digital-form-builder.git $form_runner_folder
   cd $form_runner_folder
   yarn install
   yarn run build:dependencies
