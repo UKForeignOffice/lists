a075829a6d68a88d07040880cd6cbb9d35509443
Bertrandmc <bemacode@gmail.com>
2021-08-31 16:38:15 +0100

    Restrict development resetDb and promoteUser routes to localhost only (#281)
    * Limit resetDb and promoteUser routes to localhost only
    
    * Bump app version

--- src/server/components/development/router.ts
+++ src/server/components/development/router.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { deployDb, resetDb, promoteUser } from "./controllers";
 import { ensureAuthenticated } from "server/components/auth";
 import { developmentRoutes } from "./routes";
+import { isLocalHost } from "server/config";
 
 export const developmentRouter = express.Router();
 
@@ -11,8 +12,7 @@ developmentRouter.get(`/development*`, ensureAuthenticated);
 // deploy db to apply future database migration changes
 developmentRouter.get(developmentRoutes.deployDb, deployDb);
 
-// TODO: once prod is ready this route should be available only on localhost
-developmentRouter.get(developmentRoutes.resetDb, resetDb);
-
-// TODO: once prod is ready this route should be available only on localhost
-developmentRouter.get(developmentRoutes.promoteUser, promoteUser);
+if (isLocalHost) {
+  developmentRouter.get(developmentRoutes.resetDb, resetDb);
+  developmentRouter.get(developmentRoutes.promoteUser, promoteUser);
+}
