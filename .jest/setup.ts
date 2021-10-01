1df25600410558c3d09bc159333e6b8dde69b5b0
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2021-10-01 17:38:25 +0100

    chore: remove unused packages

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,6 +1,5 @@
 jest.mock("server/services/logger");
 jest.mock("server/services/redis");
-jest.mock("redis", () => jest.requireActual("redis-mock"));
 
 beforeEach(() => {
   expect.hasAssertions();
