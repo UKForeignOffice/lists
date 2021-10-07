580e10f04d19151427bdecf9c7f37d555fd60e68
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2021-10-07 09:53:59 +0100

    feat: update GTM id for register pages

--- src/server/components/formRunner/install-form-runner.sh
+++ src/server/components/formRunner/install-form-runner.sh
@@ -37,8 +37,8 @@ echo "PRIVACY_POLICY_URL=https://www.gov.uk/help/privacy-notice" >> $form_runner
 echo "FEEDBACK_LINK=mailto:digitalservicesfeedback@fco.gov.uk" >> $form_runner_env_file
 echo "LOG_LEVEL=error" >> $form_runner_env_file
 if [ "$NODE_ENV" == "production" ]; then
-  echo "GTM_ID_1=GTM-KZWHG2G" >> $form_runner_env_file
-  echo "GTM_ID_2=GTM-PCZSN93" >> $form_runner_env_file
+  echo "GTM_ID_1=GTM-N5V9Z7G" >> $form_runner_env_file
+  echo "GTM_ID_2=GTM-KM3NZSZ" >> $form_runner_env_file
 fi
 
 if test -f ".env"; then
