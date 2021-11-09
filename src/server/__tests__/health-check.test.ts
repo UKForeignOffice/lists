fc0c71b93fc69f6af1abe2d02f33e6fa74caf36d
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2021-11-09 14:33:49 +0000

    feat: add more details to health check endpoint

--- src/server/__tests__/health-check.test.ts
+++ src/server/__tests__/health-check.test.ts
@@ -4,15 +4,46 @@ import { getServer } from "../server";
 
 describe("Health-check routes", () => {
   let server: Express;
+  let env: NodeJS.ProcessEnv;
 
   beforeAll(async () => {
     server = await getServer();
+
+    env = {
+      ...process.env,
+    };
   }, 30000);
+
+  afterEach(() => {
+    process.env = env;
+  });
 
   test("/health-check is responding correctly", async () => {
     const { body, status } = await request(server).get("/health-check");
+
     expect(status).toEqual(200);
-    expect(body).toEqual({ status: "OK" });
+
+    expect(body).toEqual({
+      status: "OK",
+      time: expect.any(String),
+      uptime: expect.any(Number),
+      version: "v0.0.0-development",
+    });
+  });
+
+  test("/health-check should return the version when env var is set", async () => {
+    process.env.DOCKER_TAG = "GIT_HASH_VALUE";
+
+    const { body, status } = await request(server).get("/health-check");
+
+    expect(status).toEqual(200);
+
+    expect(body).toEqual({
+      status: "OK",
+      time: expect.any(String),
+      uptime: expect.any(Number),
+      version: "GIT_HASH_VALUE",
+    });
   });
 
   test("/ping is responding correctly", async () => {
