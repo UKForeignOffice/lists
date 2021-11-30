8f02533fca54c981fb9d7108f8909fafa56aba47
Mo Gusbi <mo.gusbi@motechdevelopment.co.uk>
2021-11-30 14:32:32 +0000

    fix: google tag manager content security policy

--- src/server/components/formRunner/install-form-runner.sh
+++ src/server/components/formRunner/install-form-runner.sh
@@ -13,12 +13,12 @@ then
   echo "Form Runner Already Installed"
 else
   echo "Installing Form Runner"
-  git clone --depth 1 --branch 3.14.2-rc.815 https://github.com/XGovFormBuilder/digital-form-builder.git $form_runner_folder
+  git clone --depth 1 --branch 3.16.1-rc.826 https://github.com/XGovFormBuilder/digital-form-builder.git $form_runner_folder
   cd $form_runner_folder
   yarn install
   yarn run build:dependencies
   yarn runner build
-  
+
   # cleanup
   rm -rf ./designer
   rm -rf ./docs
@@ -54,4 +54,3 @@ echo "Form Runner .env Created Successfully"
 # copy forms jsons
 cp -a "$forms_json_folder/." $form_runner_forms_folder
 echo "Forms JSON Files Copied Successfully"
- 
