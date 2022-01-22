5c9f11024bcbc6a0f66870412f97bde8223a8ad0
Ali Salaman <ali@cautionyourblast.com>
2022-01-22 22:34:22 +0000

    feat: added rate limiting and CSRF protection

--- src/server/components/auth/helpers.ts
+++ src/server/components/auth/helpers.ts
@@ -3,6 +3,7 @@ import { configurePassport } from "./passport";
 import { authRoutes } from "./routes";
 import { authRouter } from "./router";
 import { configureExpressSession } from "./express-session";
+import csrf from "csurf";
 
 export function ensureAuthenticated(
   req: Request,
@@ -33,4 +34,12 @@ export async function initAuth(server: Express): Promise<void> {
   await configureExpressSession(server);
   await configurePassport(server);
   server.use(authRouter);
+}
+
+export async function initCSRF(server: Express): Promise<void> {
+  server.use(csrf({ cookie: false }));
+  server.use(function(req, res, next) {
+    res.locals._csrf = req.csrfToken();
+    next();
+  });
 }
