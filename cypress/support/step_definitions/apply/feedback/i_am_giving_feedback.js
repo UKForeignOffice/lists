9396bee57df68854a42b549dda769c9c3afd58c5
jenbutongit <jen+git@cautionyourblast.com>
2022-02-08 20:31:49 +0000

    fix: tests

--- cypress/support/step_definitions/apply/feedback/i_am_giving_feedback.js
+++ cypress/support/step_definitions/apply/feedback/i_am_giving_feedback.js
@@ -2,4 +2,6 @@ const base = "http://localhost:3000/application/feedback";
 
 Given("I am giving feedback", () => {
   cy.visit(base);
+  cy.findByRole("button", { name: "Reject analytics cookies" }).click();
+  cy.findByRole("button").click();
 });
