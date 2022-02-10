7936610375f475432c29f7426a9e993bec9e0db3
Ali Salaman <ali@cautionyourblast.com>
2022-02-10 20:00:46 +0000

    chore: added data population to prisma migrate script

--- src/server/models/db/migrations/20220210163511_list_item_list_id/migration.sql
+++ src/server/models/db/migrations/20220210163511_list_item_list_id/migration.sql
@@ -1,2 +1,22 @@
 -- AlterTable
 ALTER TABLE "ListItem" ADD COLUMN     "listId" INTEGER NOT NULL DEFAULT -1;
+
+UPDATE "ListItem" listSource SET "listId" = l."id"
+FROM "ListItem" li
+         INNER JOIN "Address" addr ON li."addressId" = addr."id"
+         INNER JOIN "List" l ON l."countryId" = addr."countryId" AND l."type" = 'covidTestProviders'
+WHERE li."type" = 'covidTestProviders'
+  and l."type" = 'covidTestProviders'
+  and listSource."type" = 'covidTestProviders'
+  and li."id" = listSource."id"
+  and li."id" = listSource."id";
+
+UPDATE "ListItem" listSource SET "listId" = l."id"
+FROM "ListItem" li
+         INNER JOIN "Address" addr ON li."addressId" = addr."id"
+         INNER JOIN "List" l ON l."countryId" = addr."countryId" AND l."type" = 'lawyers'
+WHERE li."type" = 'lawyers'
+  and l."type" = 'lawyers'
+  and listSource."type" = 'lawyers'
+  and li."id" = listSource."id"
+  and li."id" = listSource."id";
