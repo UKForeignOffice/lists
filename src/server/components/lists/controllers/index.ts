62d4865d2beb1cdee5aa339e01fa8106d8acd0ea
jenbutongit <jen+git@cautionyourblast.com>
2022-03-07 17:30:44 +0000

    refactor: move listsDataIngestionController to ingest/

--- src/server/components/lists/controllers/index.ts
+++ src/server/components/lists/controllers/index.ts
@@ -11,7 +11,6 @@ import {
   getParameterValue,
   queryStringFromParams,
   preProcessParams,
-  createConfirmationLink,
   getCountryLawyerRedirectLink,
 } from "./../helpers";
 import { questions } from "./../questionnaire";
@@ -23,15 +22,9 @@ import {
   searchCovidTestProvider,
   covidTestProviderQuestionsSequence,
 } from "./../searches/covid-test-provider";
-import {
-  LawyersFormWebhookData,
-  formRunnerPostRequestSchema,
-  parseFormRunnerWebhookObject,
-  CovidTestSupplierFormWebhookData,
-} from "server/components/formRunner";
-import { sendApplicationConfirmationEmail } from "server/services/govuk-notify";
-import serviceName from "server/utils/service-name";
 import { getCSRFToken } from "server/components/cookies/helpers";
+
+export { listsDataIngestionController } from "./ingest";
 
 export async function listsPostController(
   req: Request,
@@ -166,69 +159,6 @@ export function listsResultsController(
       break;
     default:
       next();
-  }
-}
-
-export async function listsDataIngestionController(
-  req: Request,
-  res: Response
-): Promise<void> {
-  const serviceType = req.params.serviceType as ServiceType;
-  const { value, error } = formRunnerPostRequestSchema.validate(req.body);
-
-  if (!(serviceType in ServiceType)) {
-    res.status(500).send({
-      error:
-        "serviceType is incorrect, please make sure form's webhook output configuration is correct",
-    });
-    return;
-  }
-
-  if (error !== undefined) {
-    res.status(422).send({ error: error.message });
-    return;
-  }
-
-  const data = parseFormRunnerWebhookObject<
-    LawyersFormWebhookData | CovidTestSupplierFormWebhookData
-  >(value);
-
-  try {
-    const item = await listItem.createListItem(serviceType, data);
-    const { address, reference, type } = item;
-    const typeName = serviceName(type);
-
-    if (typeName !== undefined) {
-      const { country } = address;
-      const contactName = get(item.jsonData, "contactName");
-      const email =
-        get(item.jsonData, "contactEmailAddress") ??
-        get(item.jsonData, "publicEmailAddress") ??
-        get(item.jsonData, "email") ??
-        get(item.jsonData, "emailAddress");
-
-      if (email === null) {
-        throw new Error("No email address supplied");
-      }
-
-      const confirmationLink = createConfirmationLink(req, reference);
-
-      await sendApplicationConfirmationEmail(
-        contactName,
-        email,
-        typeName,
-        country.name,
-        confirmationLink
-      );
-    }
-
-    res.json({});
-  } catch (e) {
-    logger.error(`listsDataIngestionController Error: ${e.message}`);
-
-    res.status(422).send({
-      error: "Unable to process form",
-    });
   }
 }
 
