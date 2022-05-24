a4ea2947180b3c7be5874b039e3def8233dc40f5
Ali Salaman <ali@cautionyourblast.com>
2022-05-24 22:48:56 +0100

    chore: added LOCAL_HOST and SERVICE_DOMAIN environment variables to ci scripts

--- cypress/support/step_definitions/dashboard/i_am_logged_in_as_a_string.js
+++ cypress/support/step_definitions/dashboard/i_am_logged_in_as_a_string.js
@@ -16,10 +16,5 @@ Given("I am logged in as a {string}", (role) => {
   });
 
   cy.visit("localhost:3000/login");
-
-  cy.task("log", `VISITED login page, submitting enmail address to login`);
-
   cy.get("#email-address").type("ali+test@cautionyourblast.com{enter}");
-
-  cy.task("log", `SUBMITTED login form.....`);
 });
