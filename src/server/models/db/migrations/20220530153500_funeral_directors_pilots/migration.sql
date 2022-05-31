364026ccf28ac60686c8852d13f0036c56d2bf85
jenbutongit <jen+git@cautionyourblast.com>
2022-05-31 13:17:38 +0100

    adding tom as super admin for new lists

--- src/server/models/db/migrations/20220530153500_funeral_directors_pilots/migration.sql
+++ src/server/models/db/migrations/20220530153500_funeral_directors_pilots/migration.sql
@@ -7,8 +7,7 @@ INSERT INTO "Country" ("id", "createdAt", "updatedAt", "name") VALUES
     (DEFAULT, DEFAULT, now(), 'Colombia') ON CONFLICT DO NOTHING;
 
 INSERT INTO "List" ("id", "reference", "createdAt", "updatedAt", "type", "countryId", "jsonData") VALUES
-    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='India'), '{}'),
-    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='Croatia'), '{}'),
-    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='Nigeria'), '{}'),
-    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='Colombia'), '{}') ON CONFLICT DO NOTHING;
-
+    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='India'), '{"publishers":["tom.evans@fcdo.gov.uk"],"validators":["tom.evans@fcdo.gov.uk"],"administrators":["tom.evans@fcdo.gov.uk"]}'),
+    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='Croatia'), '{"publishers":["tom.evans@fcdo.gov.uk"],"validators":["tom.evans@fcdo.gov.uk"],"administrators":["tom.evans@fcdo.gov.uk"]}'),
+    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='Nigeria'), '{"publishers":["tom.evans@fcdo.gov.uk"],"validators":["tom.evans@fcdo.gov.uk"],"administrators":["tom.evans@fcdo.gov.uk"]}'),
+    (DEFAULT, gen_random_uuid(), DEFAULT, now(), 'funeralDirectors', (SELECT "id" from "Country" WHERE "Country"."name"='Colombia'), '{"publishers":["tom.evans@fcdo.gov.uk"],"validators":["tom.evans@fcdo.gov.uk"],"administrators":["tom.evans@fcdo.gov.uk"]}') ON CONFLICT DO NOTHING;
