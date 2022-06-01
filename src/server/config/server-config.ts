b9cb35daba197059f255c8bdb2cbc8367720229a
jenbutongit <jen+git@cautionyourblast.com>
2022-06-01 14:56:12 +0100

    computed

--- src/server/config/server-config.ts
+++ src/server/config/server-config.ts
@@ -14,7 +14,8 @@ export const NODE_ENV = process.env.NODE_ENV ?? "development";
 export const isProd = NODE_ENV === "production";
 export const isDev = NODE_ENV === "development";
 export const isTest = NODE_ENV === "test";
-export const isLocalHost = process.env.LOCAL_HOST === "true";
+export const isLocalHost =
+  process.env.LOCAL_HOST === "true" || SERVICE_DOMAIN.includes("localhost");
 export const isCybDev = process.env.CYB_DEV === "true" || isDev || isTest;
 export const isCISmokeTest = process.env.CI_SMOKE_TEST === "true";
 
