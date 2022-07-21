0634d129afa5b4e3d6d464f57b078d35bea3dd63
Jen Duong <jen+git@cautionyourblast.com>
2022-07-21 16:55:59 +0000

    lint

--- src/server/components/proxyMiddleware/index.ts
+++ src/server/components/proxyMiddleware/index.ts
@@ -1,19 +1,18 @@
 import { Express } from "express";
- import proxy from "express-http-proxy";
- import { FORM_RUNNER_URL, isProd } from "server/config";
+import proxy from "express-http-proxy";
+import { FORM_RUNNER_URL, isProd } from "server/config";
 
- /**
-  * Proxy middleware for the form runner
-  * @param app Express app
-  * Important: this middleware must be added before body and cookie parsers middlewares
-  */
- export function configureFormRunnerProxyMiddleware(server: Express): void {
+/**
+ * Proxy middleware for the form runner
+ * @param app Express app
+ * Important: this middleware must be added before body and cookie parsers middlewares
+ */
+export function configureFormRunnerProxyMiddleware(server: Express): void {
   if (isProd) {
     server.set("trust proxy", ["loopback", "linklocal", "uniquelocal"]);
   } else {
     server.set("trust proxy", false);
   }
-
 
   server.use(
     `/application/*`,
@@ -27,23 +26,18 @@ import { Express } from "express";
         }
         return proxyResData
           .toString("utf8")
-          .replace(
-            /(href|src|value)=('|")\/([^'"]+)/g,
-            `$1=$2/application/$3`
-          )
+          .replace(/(href|src|value)=('|")\/([^'"]+)/g, `$1=$2/application/$3`);
       },
       userResHeaderDecorator(headers, _userReq, userRes) {
         if (userRes.statusCode === 302) {
           return {
             ...headers,
-            location: `/application${headers.location}`,
+            location: `/application${headers.location}`
           };
         }
 
         return headers;
-      },
-    
+      }
     })
   );
- }
- 
+}
