5bc88e2a2f12a2b49cde61d895d0974a9c465adf
Richard Oliver Bray <richardoliverbray@Richards-MacBook-Pro-2.local>
2022-09-30 13:56:19 +0100

    refactor: undo automatic formatting

--- src/server/components/dashboard/router.ts
+++ src/server/components/dashboard/router.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express from "express";
-import { ensureAuthenticated, ensureUserIsSuperAdmin } from "server/components/auth";
+import {
+  ensureAuthenticated,
+  ensureUserIsSuperAdmin,
+} from "server/components/auth";
 import {
   listsController,
   feedbackController,
@@ -8,7 +11,7 @@ import {
   startRouteController,
   usersListController,
   usersEditController,
-  listsItemsController,
+  listsItemsController
 } from "./controllers";
 import { dashboardRoutes } from "./routes";
 import { csrfRequestHandler } from "server/components/cookies/helpers";
@@ -20,7 +23,7 @@ import {
   listItemPostController,
   listItemPublishController,
   listItemRequestChangeController,
-  listItemUpdateController,
+  listItemUpdateController
 } from "server/components/dashboard/listsItems/controllers";
 import { redirectIfUnauthorised } from "server/components/dashboard/listsItems/helpers";
 
@@ -30,12 +33,26 @@ dashboardRouter.get(`${dashboardRoutes.start}*`, ensureAuthenticated);
 dashboardRouter.get(dashboardRoutes.start, startRouteController);
 
 // Users
-dashboardRouter.get(dashboardRoutes.usersList, csrfRequestHandler, ensureUserIsSuperAdmin, usersListController);
-dashboardRouter.all(dashboardRoutes.usersEdit, csrfRequestHandler, ensureUserIsSuperAdmin, usersEditController);
+dashboardRouter.get(
+  dashboardRoutes.usersList,
+  csrfRequestHandler,
+  ensureUserIsSuperAdmin,
+  usersListController
+);
+dashboardRouter.all(
+  dashboardRoutes.usersEdit,
+  csrfRequestHandler,
+  ensureUserIsSuperAdmin,
+  usersEditController
+);
 
 // lists
 dashboardRouter.get(dashboardRoutes.lists, csrfRequestHandler, listsController);
-dashboardRouter.all(dashboardRoutes.listsEdit, csrfRequestHandler, listsEditController);
+dashboardRouter.all(
+  dashboardRoutes.listsEdit,
+  csrfRequestHandler,
+  listsEditController
+);
 dashboardRouter.get(
   dashboardRoutes.listsItems,
   csrfRequestHandler,
@@ -45,55 +62,18 @@ dashboardRouter.get(
 );
 
 // list items
-dashboardRouter.get(
-  dashboardRoutes.listsItem,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemGetController
-);
-dashboardRouter.post(
-  dashboardRoutes.listsItemDelete,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemDeleteController
-);
-dashboardRouter.post(
-  dashboardRoutes.listsItem,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemPostController
-);
-dashboardRouter.post(
-  dashboardRoutes.listsItemPublish,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemPublishController
-);
-dashboardRouter.post(
-  dashboardRoutes.listsItemRequestChanges,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemRequestChangeController
-);
-dashboardRouter.post(
-  dashboardRoutes.listsItemUpdate,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemUpdateController
-);
-dashboardRouter.post(
-  dashboardRoutes.listsItemPin,
-  csrfRequestHandler,
-  listItemEditRequestValidation,
-  redirectIfUnauthorised,
-  listItemPinController
-);
+dashboardRouter.get(dashboardRoutes.listsItem, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemGetController);
+dashboardRouter.post(dashboardRoutes.listsItemDelete, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemDeleteController);
+dashboardRouter.post(dashboardRoutes.listsItem, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemPostController);
+dashboardRouter.post(dashboardRoutes.listsItemPublish, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemPublishController);
+dashboardRouter.post(dashboardRoutes.listsItemRequestChanges, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemRequestChangeController);
+dashboardRouter.post(dashboardRoutes.listsItemUpdate, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemUpdateController);
+dashboardRouter.post(dashboardRoutes.listsItemPin, csrfRequestHandler, listItemEditRequestValidation, redirectIfUnauthorised, listItemPinController);
 
 // feedback
-dashboardRouter.get(dashboardRoutes.feedback, csrfRequestHandler, ensureUserIsSuperAdmin, feedbackController);
+dashboardRouter.get(
+  dashboardRoutes.feedback,
+  csrfRequestHandler,
+  ensureUserIsSuperAdmin,
+  feedbackController
+);
