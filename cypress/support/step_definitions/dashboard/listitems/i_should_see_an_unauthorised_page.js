55c8b1af5c7a8b4d1fc04207d9d652774b6f305c
Ali Salaman <ali@cautionyourblast.com>
2022-10-15 00:40:48 +0100

    fixed smoke test

--- cypress/support/step_definitions/dashboard/listitems/i_should_see_an_unauthorised_page.js
+++ cypress/support/step_definitions/dashboard/listitems/i_should_see_an_unauthorised_page.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
 Then("I should see an unauthorised page", () => {
-  cy.findByText("User does not have publishing rights on this list");
+  cy.findByText("User is not authorized to access this list.");
 });
