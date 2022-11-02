e7e8bc666020e3d39070e798ffe4ec4691a72271
Richard Oliver Bray <richardbray89@gmail.com>
2022-11-02 17:05:34 +0000

    chore: add remaining pages for annual review confirmation

--- src/server/components/annual-review/router.ts
+++ src/server/components/annual-review/router.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import * as AnnualReviewController from "./controller";
-// import { csrfRequestHandler } from "server/components/cookies/helpers";
-
+import { csrfRequestHandler } from "server/components/cookies/helpers";
 
 const annualReviewRouter = express.Router();
 
-annualReviewRouter.get("/annual-review/confirm/:listItemRef", AnnualReviewController.confirm);
+annualReviewRouter.get(
+  "/annual-review/confirm/:listItemRef",
+  csrfRequestHandler,
+  AnnualReviewController.confirmGetController
+);
+annualReviewRouter.post("/annual-review/confirm/:listItemRef", AnnualReviewController.confirmPostController);
+annualReviewRouter.get(
+  "/annual-review/declaration/:listItemRef",
+  csrfRequestHandler,
+  AnnualReviewController.declarationGetController
+);
+annualReviewRouter.post("/annual-review/declaration/:listItemRef", AnnualReviewController.declarationPostController);
+annualReviewRouter.get("/annual-review/submitted", AnnualReviewController.submittedGetController);
 
 export default annualReviewRouter;
