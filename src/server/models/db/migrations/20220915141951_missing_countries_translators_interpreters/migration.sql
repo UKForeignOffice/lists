1cb1653e8b8dcbe6a4c83ba008acebd02b7eef6f
Ali Salaman <ali@cautionyourblast.com>
2022-11-15 14:49:29 +0000

    changed email address on migration script

--- src/server/models/db/migrations/20220915141951_missing_countries_translators_interpreters/migration.sql
+++ src/server/models/db/migrations/20220915141951_missing_countries_translators_interpreters/migration.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO "List" ("reference", "updatedAt", type, "countryId", "jsonData")
 
-SELECT gen_random_uuid(), NOW(), 'translatorsInterpreters', missingCountries."id", '{"publishers": ["ali@cautionyourblast.com","tom.evans@fcdo.gov.uk","rosalyn.vaughan@fcdo.gov.uk"], "validators": ["ali@cautionyourblast.com","tom.evans@fcdo.gov.uk","rosalyn.vaughan@fcdo.gov.uk"], "administrators": ["ali@cautionyourblast.com","tom.evans@fcdo.gov.uk","rosalyn.vaughan@fcdo.gov.uk"]}'
+SELECT gen_random_uuid(), NOW(), 'translatorsInterpreters', missingCountries."id", '{"publishers": ["ali.salaman@fcdo.gov.uk","tom.evans@fcdo.gov.uk","rosalyn.vaughan@fcdo.gov.uk"], "validators": ["ali.salaman@fcdo.gov.uk","tom.evans@fcdo.gov.uk","rosalyn.vaughan@fcdo.gov.uk"], "administrators": ["ali.salaman@fcdo.gov.uk","tom.evans@fcdo.gov.uk","rosalyn.vaughan@fcdo.gov.uk"]}'
 
 FROM (SELECT "Country"."id" FROM (SELECT * from "List" WHERE "type" = 'translatorsInterpreters') as countryIdsForTranslators
 
