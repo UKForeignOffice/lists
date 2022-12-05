e2675c0ddbdf5010c2ef1651e61e52c9a59dd132
jenbutongit <jen+git@cautionyourblast.com>
2022-12-05 14:43:25 +0000

    fix migration

--- src/server/models/db/migrations/20221130220000_migrate_publisher_and_validator_role/migration.sql
+++ src/server/models/db/migrations/20221130220000_migrate_publisher_and_validator_role/migration.sql
@@ -1,5 +1,9 @@
--- Migrate list.jsonData.publishers, list.jsonData.administrators and Migrate list.jsonData.validators to users
+-- Migrate list.jsonData.publishers, list.jsonData.administrators and Migrate list.jsonData.validators to just users
 
 update "List"
 set "jsonData" = jsonb_set(cast("jsonData" as jsonb), '{users}',
-                           (select distinct (coalesce(("jsonData" -> 'publishers'), '[]') || coalesce("jsonData" -> 'validators', '[]')) || coalesce("jsonData" -> 'administrators', '[]')), true);
+                           (select distinct (
+                            coalesce("jsonData" -> 'publishers', '[]'::jsonb)
+                            || coalesce("jsonData" -> 'validators', '[]'::jsonb)
+                            || coalesce("jsonData" -> 'administrators', '[]'::jsonb)
+                           )), true);
