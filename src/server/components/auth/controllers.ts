87468838336af8e712fab76ac109b58c369b3964
Richard Oliver Bray <richardbray89@gmail.com>
2022-12-08 17:55:50 +0000

    chore: update types for passport and logout method

--- src/server/components/auth/controllers.ts
+++ src/server/components/auth/controllers.ts
@@ -68,7 +68,7 @@ export async function postLoginController(
   }
 }
 
-export function getLogoutController(req: Request, res: Response): void {
-  req.logout();
-  res.redirect("/login");
+export function getLogoutController(req: Request, res: Response, next: NextFunction): void {
+  const logoutCallback = (err: Error) => (err ? next(err) : res.redirect("/login"));
+  req.logout(logoutCallback);
 }
