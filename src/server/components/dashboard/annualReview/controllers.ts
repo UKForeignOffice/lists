9ecc5fdc5216d8ab7c6365d5898e97d2793ee029
Richard Oliver Bray <richardbray89@gmail.com>
2022-12-13 16:51:51 +0000

    chore: uncomment commented code

--- src/server/components/dashboard/annualReview/controllers.ts
+++ src/server/components/dashboard/annualReview/controllers.ts
@@ -80,14 +80,14 @@ async function updateNewAnnualReviewDate(req: Request, res: Response): Promise<v
 
   await updateAnnualReviewDate(listId, newAnnualReviewDateFormatted.toISOString());
 
-  // for (const emailAddress of list.jsonData.users ?? []) {
-  //   await sendAnnualReviewDateChangeEmail({
-  //     emailAddress,
-  //     serviceType: startCase(list.type),
-  //     country: list.country!.name!,
-  //     annualReviewDate,
-  //   });
-  // }
+  for (const emailAddress of list.jsonData.users ?? []) {
+    await sendAnnualReviewDateChangeEmail({
+      emailAddress,
+      serviceType: startCase(list.type),
+      country: list.country!.name!,
+      annualReviewDate,
+    });
+  }
 
   req.flash("successBannerHeading", "Success");
   req.flash("successBannerMessage", "Annual review date updated successfully");
