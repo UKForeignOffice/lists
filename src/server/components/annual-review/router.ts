6b577a872facb8c2fb5001aed2b87edecb1a6386
Richard Oliver Bray <richardbray89@gmail.com>
2022-12-14 16:05:16 +0000

    chore: address more PR comments

--- src/server/components/annual-review/router.ts
+++ src/server/components/annual-review/router.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import express from "express";
 import * as AnnualReviewController from "./controller";
 import { csrfRequestHandler } from "server/components/cookies/helpers";
@@ -11,6 +12,5 @@ annualReviewRouter.post("/annual-review/confirm/:listItemRef", AnnualReviewContr
 annualReviewRouter.get("/annual-review/declaration/:listItemRef", AnnualReviewController.declarationGetController);
 annualReviewRouter.post("/annual-review/declaration/:listItemRef", AnnualReviewController.declarationPostController);
 annualReviewRouter.get("/annual-review/submitted", AnnualReviewController.submittedGetController);
-annualReviewRouter.get("/annual-review/error", AnnualReviewController.errorGetController);
 
 export default annualReviewRouter;
