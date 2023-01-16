402b65aa797310e82928c8a612ecfc868193b065
jenbutongit <jen+git@cautionyourblast.com>
2023-01-16 11:34:19 +0000

    fix: sworn render

--- src/server/components/dashboard/listsItems/controllers.ts
+++ src/server/components/dashboard/listsItems/controllers.ts
@@ -27,10 +27,17 @@ function mapUpdatedAuditJsonDataToListItem(
   listItem: ListItemGetObject | ListItem,
   updatedJsonData: ListItemJsonData
 ): ListItemJsonData {
+  /**
+   * Cherry-picked from origin/feat/1541-sworn-list-fix
+   */
+  const swornTranslatorFields =
+    listItem.type === "translatorsInterpreters" ? ["swornInterpretations", "swornTranslations"] : [];
+  const jsonData = (listItem as ListItemGetObject).jsonData;
+
   return Object.assign(
     {},
     listItem.jsonData,
-    ...Object.keys((listItem as ListItemGetObject).jsonData).map(
+    ...[...Object.keys(jsonData), ...swornTranslatorFields].map(
       (k) => k in updatedJsonData && { [k]: updatedJsonData[k] }
     )
   );
@@ -47,7 +54,7 @@ export async function listItemGetController(req: Request, res: ListItemRes): Pro
     };
   }
   const list = res.locals.list!;
-  const listItem = res.locals.listItem!;
+  const listItem = res.locals.listItem;
   const userId = req.user?.userData.id;
 
   let requestedChanges;
