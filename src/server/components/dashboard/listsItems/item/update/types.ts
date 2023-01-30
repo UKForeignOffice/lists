5f4dade9e86ddc676926d6b28e4eb473b20e0351
jenbutongit <jen+git@cautionyourblast.com>
2023-01-30 19:55:57 +0000

    remove unused

--- src/server/components/dashboard/listsItems/item/update/types.ts
+++ src/server/components/dashboard/listsItems/item/update/types.ts
@@ -1,6 +1,3 @@
-import { NextFunction, Request, RequestHandler } from "express";
-import { ListItemRes } from "server/components/dashboard/listsItems/types";
-
 export type Action =
   | "publish"
   | "updateNew"
