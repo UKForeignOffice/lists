1accd3d42e8b591089ddf6c4e146fdf161c3f088
Ali Salaman <ali@cautionyourblast.com>
2023-01-30 16:09:09 +0000

    email sent to POST after annual review completed by provider

---
+++ src/server/components/annual-review/helpers.ts
@@ -0,0 +1,19 @@
+import { findListById } from "server/models/list";
+import { sendAnnualReviewCompletedEmail } from "server/services/govuk-notify";
+import { lowerCase, startCase } from "lodash";
+
+export async function sendAnnualReviewCompletedEmailForList(listId: number) {
+  const list = await findListById(listId);
+
+  if (list?.jsonData?.users) {
+    await Promise.all(
+      list.jsonData.users.map(async (postEmailAddress: string) => {
+        await sendAnnualReviewCompletedEmail(
+          postEmailAddress,
+          lowerCase(startCase(list.type)),
+          list?.country?.name ?? ""
+        );
+      })
+    );
+  }
+}
