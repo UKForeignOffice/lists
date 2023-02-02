3ca4b988dcaf426ecb0027c2ac33d7466baad0b0
Jen Duong <duong.jenniferm@gmail.com>
2023-02-02 10:02:30 +0000

    Revert "Fix/allowed domains"

--- src/server/components/auth/__tests__/controllers.test.ts
+++ src/server/components/auth/__tests__/controllers.test.ts
@@ -1,4 +1,9 @@
-import { getLoginController, postLoginController, getLogoutController, authController } from "../controllers";
+import {
+  getLoginController,
+  postLoginController,
+  getLogoutController,
+  authController,
+} from "../controllers";
 import * as tokenService from "../json-web-token";
 import * as notifyService from "server/services/govuk-notify";
 import * as userModel from "server/models/user";
@@ -51,7 +56,9 @@ describe("Auth Module", () => {
   }
 
   function spyCreateAuthenticationPath(): any {
-    return jest.spyOn(tokenService, "createAuthenticationPath").mockResolvedValue("/login?token=123Token");
+    return jest
+      .spyOn(tokenService, "createAuthenticationPath")
+      .mockResolvedValue("/login?token=123Token");
   }
 
   describe("getLoginController", () => {
@@ -78,7 +85,7 @@ describe("Auth Module", () => {
 
   describe("postLoginController", () => {
     test("authentication request is successful when email address is gov.uk", (done) => {
-      const emailAddress = "person@fcdo.gov.uk";
+      const emailAddress = "person@depto.gov.uk";
       req.body.emailAddress = emailAddress;
 
       spySendAuthenticationEmail();
@@ -93,7 +100,7 @@ describe("Auth Module", () => {
     });
 
     test("sendAuthenticationEmail is called with correct parameters", (done) => {
-      const email = "person@fcdo.gov.uk";
+      const email = "person@depto.gov.uk";
       const sendEmailSpy = spySendAuthenticationEmail();
       const createAuthTokenSpy = spyCreateAuthenticationPath();
       req.body.emailAddress = email;
@@ -102,13 +109,16 @@ describe("Auth Module", () => {
 
       setTimeout(() => {
         expect(createAuthTokenSpy).toHaveBeenCalledWith({ email });
-        expect(sendEmailSpy).toHaveBeenCalledWith(email, "https://test-domain/login?token=123Token");
+        expect(sendEmailSpy).toHaveBeenCalledWith(
+          email,
+          "https://test-domain/login?token=123Token"
+        );
         done();
       });
     });
 
     test("next function is called when sendAuthenticationEmail rejects", (done) => {
-      const emailAddress = "person@fcdo.gov.uk";
+      const emailAddress = "person@depto.gov.uk";
       req.body.emailAddress = emailAddress;
 
       spySendAuthenticationEmail(true);
@@ -130,7 +140,7 @@ describe("Auth Module", () => {
     });
 
     test("authLink is not logged outside localhost", (done) => {
-      req.body.emailAddress = "person@fcdo.gov.uk";
+      req.body.emailAddress = "person@depto.gov.uk";
 
       spyCreateAuthenticationPath();
       spySendAuthenticationEmail();
@@ -148,7 +158,7 @@ describe("Auth Module", () => {
       assign(serverConfig, { isLocalHost: true });
       jest.resetModules();
 
-      req.body.emailAddress = "person@fcdo.gov.uk";
+      req.body.emailAddress = "person@depto.gov.uk";
 
       spyCreateAuthenticationPath();
       spySendAuthenticationEmail();
@@ -175,12 +185,14 @@ describe("Auth Module", () => {
   });
 
   describe("authController", () => {
-    const email = "person@fcdo.gov.uk";
+    const email = "person@depto.gov.uk";
     let spyFindUserByEmail: any;
     let spyCreateUser: any;
 
     beforeEach(() => {
-      spyFindUserByEmail = jest.spyOn(userModel, "findUserByEmail").mockResolvedValue(undefined);
+      spyFindUserByEmail = jest
+        .spyOn(userModel, "findUserByEmail")
+        .mockResolvedValue(undefined);
       spyCreateUser = jest.spyOn(userModel, "createUser").mockResolvedValue({
         email,
         jsonData: {
@@ -199,7 +211,7 @@ describe("Auth Module", () => {
 
           setTimeout(() => {
             expect(req.logIn.mock.calls[0][0]).toEqual({
-              email: "person@fcdo.gov.uk",
+              email: "person@depto.gov.uk",
               jsonData: {
                 roles: [],
               },
@@ -227,7 +239,7 @@ describe("Auth Module", () => {
     test.skip("authentication redirects to session.returnTo", (done) => {
       tokenService
         .createAuthenticationPath({
-          email: "person@fcdo.gov.uk",
+          email: "person@depto.gov.uk",
         })
         .then((authPath) => {
           req.url = `http://localhost${authPath}`;
@@ -246,7 +258,7 @@ describe("Auth Module", () => {
 
     test("authentication fails when token is invalid", async () => {
       const authPath: any = await tokenService.createAuthenticationPath({
-        email: "person@fcdo.gov.uk",
+        email: "person@depto.gov.uk",
       });
 
       req.url = `http://localhost${authPath}MAKEINVALID`;
