81c859ee9100054d1880034be7a490405208fe37
Ali Salaman <ali@cautionyourblast.com>
2023-02-09 00:09:38 +0000

    cleaned up imports

--- src/server/components/dashboard/listsItems/item/update/actionHandlers/requestChanges.ts
+++ src/server/components/dashboard/listsItems/item/update/actionHandlers/requestChanges.ts
@@ -4,7 +4,7 @@ import { initialiseFormRunnerSession } from "server/components/formRunner/helper
 import { getListItemContactInformation } from "server/models/listItem/providers/helpers";
 import serviceName from "server/utils/service-name";
 import { sendEditDetailsEmail } from "server/services/govuk-notify";
-import { AuditEvent, ListItem, Status } from "@prisma/client";
+import { AuditEvent, Status } from "@prisma/client";
 import { prisma } from "server/models/db/prisma-client";
 import { EVENTS } from "server/models/listItem/listItemEvent";
 import { recordListItemEvent } from "server/models/audit";
