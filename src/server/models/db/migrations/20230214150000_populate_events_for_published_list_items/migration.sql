d5735edc24ee0dcce01cc42bddb60eb2a3690e7d
Ali Salaman <ali@cautionyourblast.com>
2023-02-14 17:23:56 +0000

    feat: added migration script to add Event records for older listItem records that were published before the Event table was introduced

---
+++ src/server/models/db/migrations/20230214150000_populate_events_for_published_list_items/migration.sql
@@ -0,0 +1,11 @@
+-- populate Event records for list items in PUBLISHED status that were published before the Event table was introduced
+
+INSERT INTO "Event" ("listItemId", "type", "jsonData")
+select "ListItem"."id" as "listItemId",
+       'PUBLISHED'     as "type",
+       concat('{"eventName": "publish"}') ::json as "jsonData"
+from "ListItem"
+  left outer join "Event" on "Event"."listItemId" = "ListItem"."id" and "Event"."type" = 'PUBLISHED'
+where "ListItem"."status" = 'PUBLISHED'
+group by "ListItem"."id"
+having count("Event"."id") = 0
