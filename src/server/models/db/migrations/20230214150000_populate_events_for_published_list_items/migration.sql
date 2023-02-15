0a11d3e9a6d15459befc068a78af43608dfce7a6
Ali Salaman <ali@cautionyourblast.com>
2023-02-15 16:34:16 +0000

    feat: updated migration script for populating missing event records for published list items to derive from Audit data

--- src/server/models/db/migrations/20230214150000_populate_events_for_published_list_items/migration.sql
+++ src/server/models/db/migrations/20230214150000_populate_events_for_published_list_items/migration.sql
@@ -1,12 +1,21 @@
--- populate Event records for list items in PUBLISHED status that were published before the Event table was introduced
+-- create a CTE (temp table) joining Audit to Events. Returns a row when Audit is missing a matching (publish) event.
+-- Only select the relevant columns and rename them to match `Event` insert.
+with AuditWithoutMatchingListItemEvent as (select (a."jsonData" ->> 'itemId')::integer as "listItemId",
+                                             a."createdAt"                        as time,
+  a."jsonData"
+from "Audit" a
+  left outer join "Event" e on (a."jsonData" ->> 'itemId')::integer = e."listItemId"
+where e.id is null
+  and a."jsonData" ->> 'eventName' = 'publish'),
 
-INSERT INTO "Event" ("listItemId", "time", "type", "jsonData")
-select "ListItem"."id"        as "listItemId",
-       "ListItem"."updatedAt" as "time",
-       'PUBLISHED'            as "type",
-       concat('{"eventName": "publish"}') ::json as "jsonData"
-from "ListItem"
-  left outer join "Event" on "Event"."listItemId" = "ListItem"."id" and "Event"."type" = 'PUBLISHED'
-where "ListItem"."status" = 'PUBLISHED'
-group by "ListItem"."id"
-having count("Event"."id") = 0
+-- Create a CTE (temp table) joining the above audits to a ListItem. Returns when a row has a ListItem. (i.e. ListItem hasn't been deleted)
+-- Only select the relevant columns (from above).
+  AuditsToMigrate as (select a.*
+from AuditWithoutMatchingListItemEvent a
+  left outer join "ListItem" li on a."listItemId" = li.id
+where li.id is not null)
+
+insert
+into "Event" ("listItemId", "time", "type", "jsonData")
+select "listItemId", time, 'PUBLISHED', "jsonData"
+from AuditsToMigrate;
