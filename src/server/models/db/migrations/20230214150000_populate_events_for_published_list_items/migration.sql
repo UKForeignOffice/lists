9d123f98e15177c9c3bf95e3773176fdc1cba8e5
Ali Salaman <ali@cautionyourblast.com>
2023-02-15 10:52:23 +0000

    feat: updated migration script for populating events for published list items to use ListItem.updatedAt for the Event.time value

--- src/server/models/db/migrations/20230214150000_populate_events_for_published_list_items/migration.sql
+++ src/server/models/db/migrations/20230214150000_populate_events_for_published_list_items/migration.sql
@@ -1,8 +1,9 @@
 -- populate Event records for list items in PUBLISHED status that were published before the Event table was introduced
 
-INSERT INTO "Event" ("listItemId", "type", "jsonData")
-select "ListItem"."id" as "listItemId",
-       'PUBLISHED'     as "type",
+INSERT INTO "Event" ("listItemId", "time", "type", "jsonData")
+select "ListItem"."id"        as "listItemId",
+       "ListItem"."updatedAt" as "time",
+       'PUBLISHED'            as "type",
        concat('{"eventName": "publish"}') ::json as "jsonData"
 from "ListItem"
   left outer join "Event" on "Event"."listItemId" = "ListItem"."id" and "Event"."type" = 'PUBLISHED'
