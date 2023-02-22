c8398e13b3ac557ed0f3125b31397e63ca84df40
Ali Salaman <ali@cautionyourblast.com>
2023-02-22 15:44:32 +0000

    added smoke tests for scheduler worker process up to annual review start

---
+++ cypress/support/step_definitions/annual-review/scheduler/the_string_key_date_email_will_be_sent_to_post.js
@@ -0,0 +1,45 @@
+Then("the {string} key date email is sent to post", function (keyDate) {
+  cy.task("db", {
+    operation: "list.findFirst",
+    variables: {
+      where: {
+        reference: "SMOKE",
+      },
+    },
+  }).then((list) => {
+    const keyDateReminders = {
+      START: "sendStartedPostEmail",
+      POST_ONE_DAY: "sendOneDayPostEmail",
+      POST_ONE_WEEK: "sendOneWeekPostEmail",
+      POST_ONE_MONTH: "sendOneMonthPostEmail",
+    };
+    cy.task("db", {
+      operation: "audit.findFirst",
+      variables: {
+        where: {
+          type: "list",
+          auditEvent: "REMINDER",
+          AND: [
+            {
+              jsonData: {
+                path: ["reminderType"],
+                equals: keyDateReminders[keyDate],
+              },
+            },
+            {
+              jsonData: {
+                path: ["annualReviewRef"],
+                equals: list.jsonData.currentAnnualReview.reference,
+              },
+            },
+          ],
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    }).then((audit) => {
+      cy.expect(audit.id).to.exist;
+    });
+  });
+});
