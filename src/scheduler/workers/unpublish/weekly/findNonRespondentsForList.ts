a1279afdcd816f093f2938fb5580b08099587ee2
jenbutongit <jen+git@cautionyourblast.com>
2023-02-23 13:06:07 +0000

    rename

--- src/scheduler/workers/unpublish/weekly/findNonRespondentsForList.ts
+++ src/scheduler/workers/unpublish/weekly/findNonRespondentsForList.ts
@@ -1,4 +1,4 @@
-import {type List} from "@prisma/client";
+import {type List, Prisma} from "@prisma/client";
 import {differenceInWeeks, parseISO, startOfDay, startOfToday} from "date-fns";
 import {prisma} from "server/models/db/prisma-client";
 import {type ListJsonData} from "server/models/types";
@@ -8,7 +8,7 @@ export async function findNonRespondentsForList(list: List) {
   const log = logger.child({listId: list.id, method: "findNonRespondentsForList" })
 
   const jsonData = list.jsonData as ListJsonData;
-  const { keyDates, reference } = jsonData.currentAnnualReview;
+  const { keyDates, reference } = jsonData.currentAnnualReview!;
   const { unpublished } = keyDates;
   const unpublishDate = startOfDay(parseISO(unpublished.UNPUBLISH));
   log.info(`unpublish date ${unpublished.UNPUBLISH}`)
@@ -16,7 +16,7 @@ export async function findNonRespondentsForList(list: List) {
   const today = startOfToday();
   const weeksUntilUnpublish = differenceInWeeks(unpublishDate, today);
   log.info(`Unpublish date is ${weeksUntilUnpublish} weeks away`)
-  const weeksBeforeUnpublishToQuery: Record<number, Date> = {
+  const weeksBeforeUnpublishToQuery: {[n: number]: string} = {
     5: unpublished.PROVIDER_FIVE_WEEKS,
     4: unpublished.PROVIDER_FOUR_WEEKS,
     3: unpublished.PROVIDER_THREE_WEEKS,
@@ -27,6 +27,18 @@ export async function findNonRespondentsForList(list: List) {
   const reminderToFind = weeksBeforeUnpublishToQuery[weeksUntilUnpublish];
   const annualReviewDate = new Date(list.nextAnnualReviewStartDate!).toISOString();
   log.info(`looking for list items to send unpublish provider reminder at ${weeksUntilUnpublish} weeks`)
+  const editedSinceAnnualReviewDate: Prisma.EventWhereInput = {
+      type: "EDITED",
+      time: {
+        gte: annualReviewDate,
+      }
+    }
+  const reminderHasBeenSent: Prisma.EventWhereInput = {
+    type: "REMINDER",
+    time: {
+      gte: reminderToFind
+    }
+  }
   const listItems = await prisma.listItem.findMany({
     where: {
       listId: list.id,
@@ -36,18 +48,8 @@ export async function findNonRespondentsForList(list: List) {
         none: {
           // no "edited" event found after annual review began
           AND: [
-            {
-              type: "EDITED",
-              time: {
-                gte: annualReviewDate,
-              }
-            },
-            {
-              type: "REMINDER",
-              time: {
-                gte: reminderToFind
-              }
-            }
+            editedSinceAnnualReviewDate,
+            reminderHasBeenSent,
           ],
         },
       },
