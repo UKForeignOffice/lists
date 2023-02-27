907923df1caa474acd73e8dbfeb171d16e072813
jenbutongit <jen+git@cautionyourblast.com>
2023-02-27 14:58:02 +0000

    debug helper

--- src/scheduler/workers/unpublish/weekly/getMetaForList.ts
+++ src/scheduler/workers/unpublish/weekly/getMetaForList.ts
@@ -1,13 +1,14 @@
 import { differenceInWeeks, parseISO, startOfDay, startOfToday } from "date-fns";
 import { List } from "server/models/types";
+import Prisma from "@prisma/client";
 
 interface Meta {
   reference: string;
   weeksUntilUnpublish: number;
 }
 
-export function getMetaForList(list: List): Meta {
-  const { jsonData } = list;
+export function getMetaForList(list: Prisma.List): Meta {
+  const { jsonData } = list as List;
   const { currentAnnualReview } = jsonData;
   const { keyDates } = currentAnnualReview;
 
