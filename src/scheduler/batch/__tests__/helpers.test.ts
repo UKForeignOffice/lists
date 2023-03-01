a080bb083f64d4c49c93f23520cac65a10234ef8
Ali Salaman <ali@cautionyourblast.com>
2023-03-01 10:50:22 +0000

    fixed failing test

--- src/scheduler/batch/__tests__/helpers.test.ts
+++ src/scheduler/batch/__tests__/helpers.test.ts
@@ -112,7 +112,7 @@ describe("Today Date", () => {
   });
   describe("today is BST timezone after 11pm, before midnight", () => {
     jest.useFakeTimers().setSystemTime(new Date("05-May-2023 23:37:22.000"));
-    const expectedDate = "2023-05-04T00:00:00.000Z";
+    const expectedDate = "2023-05-05T00:00:00.000Z";
     const actualDate = startOfDay(new Date()).toISOString();
     expect(actualDate).toBe(expectedDate);
   });
