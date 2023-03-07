7c26de11441a2f292b3326d6ff64ae5cf4814c0f
Ali Salaman <ali@cautionyourblast.com>
2023-03-07 08:53:48 +0000

    adds ANNUAL_REVIEW_OVERDUE Event when unpublishing list items

--- src/scheduler/workers/unpublish/day/changeState/unpublishListItems.ts
+++ src/scheduler/workers/unpublish/day/changeState/unpublishListItems.ts
@@ -1,18 +1,18 @@
 import { prisma } from "server/models/db/prisma-client";
 import { EVENTS } from "server/models/listItem/listItemEvent";
 
-export async function unpublishListItems(ids: number[]) {
+export async function unpublishListItems(ids: number[], reference?: string) {
   return ids.map(async (listItemId: number) => {
     return await prisma.listItem.update({
       where: {
         id: listItemId,
       },
       data: {
-        status: "UNPUBLISHED",
+        status: "ANNUAL_REVIEW_OVERDUE",
         isAnnualReview: false,
         isPublished: false,
         history: {
-          create: EVENTS.UNPUBLISHED(),
+          create: EVENTS.ANNUAL_REVIEW_OVERDUE(reference),
         },
       },
     });
