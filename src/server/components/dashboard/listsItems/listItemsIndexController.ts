5bc0e245197acbb478e6c619a4562fbfc84e45b2
jenbutongit <jen+git@cautionyourblast.com>
2023-03-14 15:35:31 +0000

    remove unused

--- src/server/components/dashboard/listsItems/listItemsIndexController.ts
+++ src/server/components/dashboard/listsItems/listItemsIndexController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request } from "express";
 import { DEFAULT_VIEW_PROPS } from "server/components/lists/constants";
 import { findIndexListItems } from "server/models/listItem/listItem";
-import { ACTIVITY_TAGS, IndexListItem, ORDER_BY, PUBLISHING_TAGS, TAGS, Tags } from "server/models/listItem/types";
+import { ACTIVITY_TAGS, ORDER_BY, PUBLISHING_TAGS, TAGS, Tags } from "server/models/listItem/types";
 import { getCSRFToken } from "server/components/cookies/helpers";
 import { ListItemRes } from "server/components/dashboard/listsItems/types";
 import * as AnnualReviewHelpers from "server/components/dashboard/annualReview/helpers";
