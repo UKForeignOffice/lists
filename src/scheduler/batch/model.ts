eac017dc3b7a4f15d9564d4086fa0e17ac5bf9dc
Richard Oliver Bray <richardbray89@gmail.com>
2023-03-23 15:00:26 +0000

    chore: fix type related issues

--- src/scheduler/batch/model.ts
+++ src/scheduler/batch/model.ts
@@ -1,10 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
+import type { List, CurrentAnnualReview, ListUpdateInput, ListJsonData } from "server/models/types";
+import type { ListItemWithHistory } from "server/components/dashboard/listsItems/types";
+
 export const prisma = new PrismaClient();
 
 export interface ListWithFirstPublishedDate {
   listId: number;
   oneYearAfterFirstPublishedDate: Date | string;
+}
+
+interface FormattedList {
+  id: number;
+  items: ListItemWithHistory[];
+  jsonData: ListJsonData;
+  nextAnnualReviewStartDate: Date | null;
 }
 
 export async function addAnnualReviewToList({ listId, oneYearAfterFirstPublishedDate }: ListWithFirstPublishedDate) {
