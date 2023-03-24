148b837d3e2bfdaa374f702b6f85f30e4da58884
jenbutongit <jen+git@cautionyourblast.com>
2023-03-24 11:22:10 +0000

    "firstPublished"

--- src/server/models/db/migrations/20230302210317_add_view_FirstPublishedOnList/migration.sql
+++ src/server/models/db/migrations/20230302210317_add_view_FirstPublishedOnList/migration.sql
@@ -2,7 +2,7 @@
 -- select distinct on (l.id), similar to group by, ensures only 1 entry appears per listId
 -- min(e.time) selects the earliest time a published event occurred (firstPublished)
 create view "FirstPublishedOnList" as (
-  select distinct on (l.id) "listId", min(e.time) over (partition by l.id) firstPublished, e.id "eventId"
+  select distinct on (l.id) "listId", min(e.time) over (partition by l.id) "firstPublished", e.id "eventId"
   from (select time, type, id, "listItemId" from "Event" where type = 'PUBLISHED') e
   inner join (select "listId", "id" from "ListItem") li ON e."listItemId" = li.id
   inner join "List" l ON li."listId" = l.id);
