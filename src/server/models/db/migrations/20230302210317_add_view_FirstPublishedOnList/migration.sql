68d64da5ca67660afaae573a822c52994e353d8f
jenbutongit <jen+git@cautionyourblast.com>
2023-03-24 18:38:25 +0000

    invert query and include list.firstPublishedOnList

--- src/server/models/db/migrations/20230302210317_add_view_FirstPublishedOnList/migration.sql
+++ src/server/models/db/migrations/20230302210317_add_view_FirstPublishedOnList/migration.sql
@@ -2,7 +2,10 @@
 -- select distinct on (l.id), similar to group by, ensures only 1 entry appears per listId
 -- min(e.time) selects the earliest time a published event occurred (firstPublished)
 create view "FirstPublishedOnList" as (
-  select distinct on (l.id) "listId", min(e.time) over (partition by l.id) "firstPublished", e.id "eventId"
-  from (select time, type, id, "listItemId" from "Event" where type = 'PUBLISHED') e
-  inner join (select "listId", "id" from "ListItem") li ON e."listItemId" = li.id
-  inner join "List" l ON li."listId" = l.id);
+ select distinct on (l.id) "listId", min(e.time) over (partition by l.id) "firstPublished", e.id "eventId", l."nextAnnualReviewStartDate"
+ from (select "id", "nextAnnualReviewStartDate" from "List") l
+          inner join (select "listId", "id" from "ListItem") li ON li."listId" = l.id
+          inner join (select time, type, id, "listItemId" from "Event" where type = 'PUBLISHED') e on li.id = "listItemId"
+        );
+
+
