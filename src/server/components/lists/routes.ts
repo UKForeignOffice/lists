3bec38633c7d9f17344a827acdccc87d5b9c73e0
Richard Oliver Bray <richardbray89@gmail.com>
2023-03-29 17:33:07 +0100

    chore: add new page for non existent lists

--- src/server/components/lists/routes.ts
+++ src/server/components/lists/routes.ts
@@ -6,6 +6,7 @@ export const listsRoutes = {
   feedback: "/feedback",
   feedbackSuccess: "/feedback/success",
   privateBeta: "/private-beta",
+  noListExists: "/no-list-exists",
   formRunnerWebhook: "/ingest/:serviceType",
   formRunnerCallback: "/ingest/:serviceType/:id",
   confirmApplication: "/confirm/:reference",
