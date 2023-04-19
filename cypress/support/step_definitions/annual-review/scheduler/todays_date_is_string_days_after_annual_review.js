f61329430d8afc22f91bc4c8ec55a33407e9381e
Richard Oliver Bray <richardbray89@gmail.com>
2023-04-19 14:10:53 +0100

    refactor(annual-review): remove worker process from unpublish.feature and add it to todays_date_is_string_days_after_annual_review.js step definition file

--- cypress/support/step_definitions/annual-review/scheduler/todays_date_is_string_days_after_annual_review.js
+++ cypress/support/step_definitions/annual-review/scheduler/todays_date_is_string_days_after_annual_review.js
@@ -1,9 +1,11 @@
 import { addDays, startOfDay, subDays } from "date-fns";
 
-And("todays date is {string} days after annual review", (date) => {
+And("todays date is {string} days after annual review", async (date) => {
   const currentDate = new Date();
   const futureDate = addDays(currentDate, date);
 
   cy.clock(futureDate.getTime());
   cy.stub(Date, 'now').returns(futureDate.getTime());
+
+  await cy.task("worker");
 });
