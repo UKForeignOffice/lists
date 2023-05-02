61243444d5a2e73820af3bb008207042c57c9eed
Richard Oliver Bray <richardbray89@gmail.com>
2023-05-02 14:32:50 +0100

    refactor(cypress): remove eslint-disable comment chore(cypress): remove TOTAL_NO_UNPUBLISH_WEEKS constant

--- cypress/support/step_definitions/annual-review/scheduler/the_unpublish_reminder_email_for_{string}_days_is_sent_to_eligible_providers.js
+++ cypress/support/step_definitions/annual-review/scheduler/the_unpublish_reminder_email_for_{string}_days_is_sent_to_eligible_providers.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-const TOTAL_NO_UNPUBLISH_WEEKS = 5
 Then("the unpublish reminder email for {int} days is sent to eligible providers", async (noOfDays) => {
   cy.task("db", {
     operation: "list.findFirst",
