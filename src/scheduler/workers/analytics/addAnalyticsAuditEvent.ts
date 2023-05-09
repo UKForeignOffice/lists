27092f80b67826025ccefdde52a0e245f5076abe
jen <jen@cautionyourblast.com>
2023-05-09 12:14:48 +0100

    fix alg

--- src/scheduler/workers/analytics/addAnalyticsAuditEvent.ts
+++ src/scheduler/workers/analytics/addAnalyticsAuditEvent.ts
@@ -3,18 +3,6 @@ import { prisma } from "scheduler/workers/model";
 const truncateMessage = "... truncated due to max length";
 const MAX_BYTES = 7000 - Buffer.byteLength(truncateMessage);
 
-function getByteLength(string: string) {
-  return Buffer.byteLength(string);
-}
-
-function truncateString(string: string, maxSize: number, hasTruncated = false): string {
-  if (getByteLength(string) <= maxSize) {
-    return `${string}${hasTruncated ? truncateMessage : ""}`;
-  }
-  const sliceAmount = !hasTruncated ? maxSize : maxSize + 16;
-  return truncateString(string.slice(0, sliceAmount), maxSize, true);
-}
-
 export async function addAnalyticsAuditEvent(view: string, dataSent: any[], response: any) {
   const maxSize = MAX_BYTES - getByteLength(JSON.stringify({ data: "", response }));
   const truncatedData = truncateString(JSON.stringify(dataSent), maxSize);
@@ -30,4 +18,23 @@ export async function addAnalyticsAuditEvent(view: string, dataSent: any[], resp
       },
     },
   });
+}
+
+function getByteLength(string: string) {
+  return Buffer.byteLength(string);
+}
+
+function truncateString(string: string, maxByteLength: number, hasTruncated = false): string {
+  /**
+   * Recursive function which will keep removing characters until the string is the byte under limit.
+   * JS (node 14) does not natively support truncating/slicing by byte length, and due to UTF-16,
+   * characters may be 2-5+ bytes long, hence "estimatedMaxCharLength"
+   */
+  const stringByteLength = getByteLength(string);
+  if (stringByteLength <= maxByteLength) {
+    return `${string}${hasTruncated ? truncateMessage : ""}`;
+  }
+  const estimatedMaxCharLength = Math.ceil(maxByteLength / 2);
+  const sliceAmount = !hasTruncated ? estimatedMaxCharLength : estimatedMaxCharLength - 16;
+  return truncateString(string.slice(0, sliceAmount), maxByteLength, true);
 }
