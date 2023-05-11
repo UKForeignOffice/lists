2bf08283797f18363422a72d68abc5e393435c65
RichardBray <richardbray89@gmail.com>
2023-05-11 12:48:40 +0100

    refactor(authenticated-user.ts): make orderBy parameter optional refactor(controllers.ts): export calculateSortOrder function refactor(controllers.ts): add userIsAdmin parameter to calculateDashboardBoxes function test(authenticated-user.test.ts): change getLists test to test that an empty object is passed to prisma.listsForDashboard.findMany test(controllers.test.ts): add tests for calculateSortOrder function test(controllers.test.ts): add test for calculateDashboardBoxes function when user is an admin

--- src/server/components/dashboard/__tests__/controllers.test.ts
+++ src/server/components/dashboard/__tests__/controllers.test.ts
@@ -10,6 +10,7 @@ import {
   usersEditController,
   usersEditPostController,
   usersListController,
+  calculateSortOrder,
 } from "../controllers";
 import * as govukNotify from "../../../services/govuk-notify";
 import * as helpers from "server/components/dashboard/helpers";
@@ -266,6 +267,33 @@ describe("Dashboard Controllers", () => {
       expect(mockRes.render.mock.calls[0][1].isNewUser).toBeTruthy();
     });
   });
+
+
+  describe.only('calculateSortOrder', () => {
+    test('returns correct sort order when there are no query parameters', () => {
+      const expectedResult = [{ country: 'asc' }, { type: 'asc' }];
+      expect(calculateSortOrder({})).toEqual(expectedResult);
+    });
+
+    test("returns correct sort order when admins query parameter is not provided", () => {
+      const expectedResult = [{ country: "asc" }, { type: "asc" }];
+      expect(calculateSortOrder({ jsonData: "asc" })).toEqual(expectedResult);
+    });
+
+    test('returns correct sort order with desc sort direction', () => {
+      const queryParamSortOrder: { admins: "desc" } = { admins: "desc" };
+
+      const expectedResult = [{ admins: 'desc' }, { country: 'asc' }, { type: 'asc' }];
+      expect(calculateSortOrder(queryParamSortOrder)).toEqual(expectedResult);
+    });
+
+    test('returns correct sort order with asc sort direction', () => {
+      const queryParamSortOrder: { admins: "asc" } = { admins: 'asc' };
+      const expectedResult = [{ admins: 'asc' }, { country: 'asc' }, { type: 'asc' }];
+      expect(calculateSortOrder(queryParamSortOrder)).toEqual(expectedResult);
+    });
+  });
+
 
   describe("listsItemsController", () => {
     let listItems: any[];
