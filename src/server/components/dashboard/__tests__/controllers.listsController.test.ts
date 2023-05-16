ae646af1eef10e050e57a73d65f17b30af993bc9
jen <jen@cautionyourblast.com>
2023-05-16 17:20:16 +0100

    check that unknown values are stripped

--- src/server/components/dashboard/__tests__/controllers.listsController.test.ts
+++ src/server/components/dashboard/__tests__/controllers.listsController.test.ts
@@ -12,15 +12,18 @@ describe("calculateSortOrder", () => {
   });
 
   test("returns correct sort order with desc sort direction", () => {
-    const queryParamSortOrder: { admins: "desc" } = { admins: "desc" };
-
     const expectedResult = [{ admins: "desc" }, { country: "asc" }, { type: "asc" }];
-    expect(calculateSortOrder(queryParamSortOrder)).toEqual(expectedResult);
+    expect(calculateSortOrder({ admins: "desc" })).toEqual(expectedResult);
   });
 
   test("returns correct sort order with asc sort direction", () => {
-    const queryParamSortOrder: { admins: "asc" } = { admins: "asc" };
     const expectedResult = [{ admins: "asc" }, { country: "asc" }, { type: "asc" }];
-    expect(calculateSortOrder(queryParamSortOrder)).toEqual(expectedResult);
+    expect(calculateSortOrder({ admins: "desc" })).toEqual(expectedResult);
+  });
+
+  test("unknown values are stripped", () => {
+    expect(calculateSortOrder({ admins: "abcef" })).toEqual([{ country: "asc" }, { type: "asc" }]);
+    expect(calculateSortOrder({ country: "abcef" })).toEqual([{ country: "asc" }, { type: "asc" }]);
+    expect(calculateSortOrder({ type: "abcef" })).toEqual([{ country: "asc" }, { type: "asc" }]);
   });
 });
