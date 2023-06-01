f7525df10a3fb717979a12fd0d1cb7bf7f394b8a
RichardBray <richardbray89@gmail.com>
2023-06-01 10:29:42 +0100

    refactor: remove eventId from related links in migration.sql and list.ts files

--- src/server/models/db/migrations/20230531144729_add_related_links/migration.sql
+++ src/server/models/db/migrations/20230531144729_add_related_links/migration.sql
@@ -5,59 +5,49 @@ set "jsonData" = jsonb_set("jsonData", '{relatedLinks}',
         then coalesce("jsonData"->'relatedLinks', '[]') || '[
             {
                 "url": "gov.uk/guidance/arrested-or-detained-abroad",
-                "text": "Arrested or detained abroad",
-                "eventId": "123"
+                "text": "Arrested or detained abroad"
             },
             {
                 "url": "gov.uk/guidance/what-to-do-after-a-british-national-dies-abroad",
-                "text": "What to do after a British national dies abroad",
-                "eventId": "123"
+                "text": "What to do after a British national dies abroad"
             },
             {
                 "url": "gov.uk/guidance/victim-of-crime-abroad",
-                "text": "Victim of crime abroad",
-                "eventId": "123"
+                "text": "Victim of crime abroad"
             }
         ]'
         when 'funeralDirectors'
         then coalesce("jsonData"->'relatedLinks', '[]') || '[
             {
                 "url": "gov.uk/guidance/what-to-do-after-a-british-national-dies-abroad",
-                "text": "What to do after a British national dies abroad",
-                "eventId": "123"
+                "text": "What to do after a British national dies abroad"
             },
             {
                 "url": "gov.uk/government/publications/international-funeral-directors-in-the-uk/list-of-uk-based-international-funeral-directors--2",
-                "text": "UK-based international funeral directors",
-                "eventId": "123"
+                "text": "UK-based international funeral directors"
             },
             {
                 "url": "gov.uk/after-a-death/organisations-you-need-to-contact-and-tell-us-once",
-                "text": "Tell us once",
-                "eventId": "123"
+                "text": "Tell us once"
             }
         ]'
         when 'translatorsInterpreters'
         then coalesce("jsonData"->'relatedLinks', '[]') || '[
             {
                 "url": "gov.uk/guidance/arrested-or-detained-abroad",
-                "text": "Arrested or detained abroad",
-                "eventId": "123"
+                "text": "Arrested or detained abroad"
             },
             {
                 "url": "gov.uk/guidance/what-to-do-after-a-british-national-dies-abroad",
-                "text": "What to do after a British national dies abroad",
-                "eventId": "123"
+                "text": "What to do after a British national dies abroad"
             },
             {
                 "url": "gov.uk/guidance/in-hospital-abroad",
-                "text": "In hospital abroad",
-                "eventId": "123"
+                "text": "In hospital abroad"
             },
             {
                 "url": "gov.uk/guidance/victim-of-crime-abroad",
-                "text": "Victim of crime abroad",
-                "eventId": "123"
+                "text": "Victim of crime abroad"
             }
         ]'
         else "jsonData"
