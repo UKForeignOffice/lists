3fe6503fdb4ac73c5aa151d65375422e16ce24f1
jen <jen@cautionyourblast.com>
2023-06-05 15:45:38 +0100

    feat: render custom related links on find journey

---
+++ src/server/components/lists/searches/helpers/getRelatedLinks.ts
@@ -0,0 +1,34 @@
+import { prisma } from "server/models/db/prisma-client";
+import { logger } from "server/services/logger";
+import { type List, ServiceType } from "shared/types";
+
+export async function getRelatedLinks( countryName: string, serviceType: ServiceType ) {
+  console.log("get related")
+  const list = await prisma.list.findFirst({
+    where: {
+      country: {
+        name: countryName,
+      },
+      type: serviceType
+    },
+    select: {
+      id: true,
+      jsonData: true,
+    },
+  });
+
+  if (!list) {
+    logger.info(`getRelatedLinks for ${countryName} ${serviceType}, list not found`);
+  }
+
+  const { jsonData } = list as List;
+  const { relatedLinks = [] } = jsonData;
+
+  if (relatedLinks.length === 0) {
+    logger.info(`getRelatedLinks for ${list!.id} (${countryName}, ${serviceType}) returned with 0 links`);
+  }
+
+  console.log("gr",relatedLinks)
+
+  return relatedLinks;
+}
