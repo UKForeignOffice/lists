dfd5996ebcc3eb42ef848666c4d95a409dd5f062
jen <jen@cautionyourblast.com>
2023-06-06 13:11:02 +0100

    lint

--- src/server/components/lists/searches/helpers/getRelatedLinks.ts
+++ src/server/components/lists/searches/helpers/getRelatedLinks.ts
@@ -1,14 +1,14 @@
 import { prisma } from "server/models/db/prisma-client";
 import { logger } from "server/services/logger";
-import { type List, ServiceType } from "shared/types";
+import type { ServiceType, List } from "shared/types";
 
-export async function getRelatedLinks( countryName: string, serviceType: ServiceType ) {
+export async function getRelatedLinks(countryName: string, serviceType: ServiceType) {
   const list = await prisma.list.findFirst({
     where: {
       country: {
         name: countryName,
       },
-      type: serviceType
+      type: serviceType,
     },
     select: {
       id: true,
@@ -18,17 +18,15 @@ export async function getRelatedLinks( countryName: string, serviceType: Service
 
   if (!list) {
     logger.info(`getRelatedLinks for ${countryName} ${serviceType}, list not found`);
-    return []
+    return [];
   }
 
   const { jsonData } = list as List;
   const { relatedLinks = [] } = jsonData;
 
   if (relatedLinks.length === 0) {
-    logger.info(`getRelatedLinks for ${list!.id} (${countryName}, ${serviceType}) returned with 0 links`);
-
+    logger.info(`getRelatedLinks for ${list.id} (${countryName}, ${serviceType}) returned with 0 links`);
   }
-
 
   return relatedLinks;
 }
