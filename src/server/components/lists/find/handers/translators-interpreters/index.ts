f867feb5e712338360ea30cf9d239cb5a0f1a5a3
jen <jen@cautionyourblast.com>
2023-06-20 15:05:38 +0100

    translator interpreters setup

---
+++ src/server/components/lists/find/handers/translators-interpreters/index.ts
@@ -0,0 +1,5 @@
+export * as languages from "./languages";
+export * as languagesSummary from "./languages-summary";
+export * as services from "./services";
+export * as servicesSummary from "./services-summary";
+export * as specialties from "./specialties";
---
+++ src/server/components/lists/find/handers/translators-interpreters/services-summary.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+
+export function get(req: Request, res: Response) {}
+
+export function post(req: Request, res: Response) {}
