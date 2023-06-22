e93a1b856d29994c6f21369fcf7b6f6d82b950a9
RichardBray <richardbray89@gmail.com>
2023-06-22 16:27:21 +0100

    chore(unpublish): add logging and delete list items unpublished by annual review if they are older than 1 year

--- src/scheduler/workers/unpublish/delete/index.ts
+++ src/scheduler/workers/unpublish/delete/index.ts
@@ -1,5 +1,6 @@
 import { addYears } from "date-fns";
 import { prisma } from "scheduler/prismaClient";
+import { schedulerLogger as logger } from "scheduler/logger";
 
 export default async function main() {
   const itemsUnpublishedByAR = await prisma.listItem.findMany({
@@ -33,9 +34,18 @@ export default async function main() {
     },
   });
 
+  logger.info(`main: ${itemsUnpublishedByAR.length} list items found that were unpublished by annual review`);
+
   for (const item of itemsUnpublishedByAR) {
     if (item.history[0].type === "UNPUBLISHED") {
       const timeLimit = addYears(item.history[0].time, 1);
+      if (new Date() > timeLimit) {
+        await prisma.listItem.delete({
+          where: {
+            id: item.id,
+          },
+        });
+      }
     }
   }
 }
