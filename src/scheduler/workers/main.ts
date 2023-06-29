30446b5fc7433d5313e3c9ddf49ffb0cb5c07623
jen <jen@cautionyourblast.com>
2023-06-29 10:55:55 +0100

    feat: disable analytics task

--- src/scheduler/workers/main.ts
+++ src/scheduler/workers/main.ts
@@ -2,7 +2,6 @@ import { main as unpublishWeeklyTask } from "./unpublish/weekly";
 import { main as unpublishDayBeforeTask } from "./unpublish/dayBefore";
 import { main as unpublishDayTask } from "./unpublish/day";
 import { processAnnualReview as processListsBeforeAndDuringStart } from "./processListsBeforeAndDuringStart/main";
-import { main as analytics } from "./analytics/main";
 import { logger } from "scheduler/logger";
 import deleteItemsAfterAYear from "./unpublish/delete";
 
@@ -15,13 +14,7 @@ async function main() {
   }
 
   // put all worker tasks to be executed here. They will be executed async (non blocking/non sequential).
-  const tasks = [
-    unpublishWeeklyTask(),
-    unpublishDayBeforeTask(),
-    unpublishDayTask(),
-    analytics(),
-    deleteItemsAfterAYear(),
-  ];
+  const tasks = [unpublishWeeklyTask(), unpublishDayBeforeTask(), unpublishDayTask(), deleteItemsAfterAYear()];
 
   return await Promise.allSettled(tasks);
 }
