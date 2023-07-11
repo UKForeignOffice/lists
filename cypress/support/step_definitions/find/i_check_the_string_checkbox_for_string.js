e67c32ab2945551d2b07e7f423c0749cff419f8d
jen <jen@cautionyourblast.com>
2023-07-11 16:02:33 +0100

    use `when` instead of and

--- cypress/support/step_definitions/find/i_check_the_string_checkbox_for_string.js
+++ cypress/support/step_definitions/find/i_check_the_string_checkbox_for_string.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-And("I {string} the {string} checkbox for {string}", (checkOrUncheck, checkButton, group) => {
+When("I {string} the {string} checkbox for {string}", (checkOrUncheck, checkButton, group) => {
   cy.findByRole("group", { name: group }).within(() => {
     cy.findByRole("checkbox", { name: checkButton })[checkOrUncheck]();
   });
