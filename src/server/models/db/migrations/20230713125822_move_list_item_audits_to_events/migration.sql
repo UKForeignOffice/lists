2df8404d27b0703a1cbcd057f10e508d9185043e
RichardBray <richardbray89@gmail.com>
2023-07-13 17:19:19 +0100

    refactor(migration.sql): Move list item audits to events table
    The migration.sql file has been modified to move list item audits to the events table. The changes include:
    
    - Renaming the "auditEvent" column to "type" in the SELECT statement.
    - Changing the value of the "type" column to "REMINDER" for all rows.
    - Updating the "jsonData" column to include additional data for the event.
    - Joining the "Audit" table with the "List" table to ensure the correct list item is selected.
    - Removing the deletion of list item audit events.
    
    This change improves the organization and structure of the database by consolidating list item audits into the events table.

--- src/server/models/db/migrations/20230713125822_move_list_item_audits_to_events/migration.sql
+++ src/server/models/db/migrations/20230713125822_move_list_item_audits_to_events/migration.sql
@@ -1,19 +1,16 @@
 -- Move Reminder audit events to Event table: (https://www.db-fiddle.com/f/pAsG9CgsiMz5YMZZo9FJ1t/0)
 -- NOTE: Event table entries contain response data from Notify emails. Since Audit doesn't have this, it won't be included when entries are moved
 INSERT INTO "Event" ("time", "listItemId", "type", "jsonData")
-SELECT  "createdAt",
-        ("jsonData"->>'itemId')::int,
-        "auditEvent",
+SELECT  a."createdAt" "createdAt",
+        (a."jsonData"->>'itemId')::int,
+        "REMINDER",
         jsonb_set(
           '{}'::jsonb,
           '{reference}',
-          to_jsonb("jsonData"->'annualReviewRef')) ||
+          to_jsonb(a."jsonData"->'annualReviewRef')) ||
           jsonb_build_object('notes', '["sendStartedProviderEmail"]') ||
           jsonb_build_object('eventName', '["reminder"]')
-FROM "Audit"
-WHERE type = 'listItem'
+FROM "Audit" a
+INNER JOIN "List" l on  (a."jsonData"->>'itemId')::int = l.id
+WHERE a.type = 'listItem'
 AND "auditEvent" = 'REMINDER';
-
--- Delete all lstItem audit events.
-DELETE FROM "Audit"
-WHERE type = 'listItem';
