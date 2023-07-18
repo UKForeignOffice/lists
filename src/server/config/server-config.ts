d5072d553761d173de2865125924e68bb68e0e98
RichardBray <richardbray89@gmail.com>
2023-07-18 17:36:10 +0100

    chore(ingestPutController.ts): add support for sending provider informed of edit email chore(server-config.ts): add providerInformedOfEdit template ID to GOVUK NOTIFY configuration

--- src/server/config/server-config.ts
+++ src/server/config/server-config.ts
@@ -39,6 +39,7 @@ export const NOTIFY = {
     editProviderDetails: process.env.GOVUK_NOTIFY_EDIT_PROVIDER_DETAILS_TEMPLATE_ID?.trim() ?? "",
     listItemUnpublished: process.env.GOVUK_NOTIFY_ITEM_UNPUBLISHED_TEMPLATE_ID?.trim() ?? "",
     contactUsApplyJourney: process.env.GOVUK_NOTIFY_CONTACT_US_APPLY_TEMPLATE_ID?.trim() ?? "",
+    providerInformedOfEdit: process.env.GOVUK_NOTIFY_PROVIDER_INFORMED_OF_EDIT_TEMPLATE_ID?.trim() ?? "",
     annualReviewNotices: {
       postOneMonth: process.env.GOVUK_NOTIFY_ANNUAL_REVIEW_POST_ONE_MONTH_NOTICE?.trim() ?? "",
       postOneWeek: process.env.GOVUK_NOTIFY_ANNUAL_REVIEW_POST_ONE_WEEK_NOTICE?.trim() ?? "",
