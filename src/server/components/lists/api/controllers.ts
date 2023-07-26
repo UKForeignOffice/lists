d97db6a0b2a2f68edac1abf80b39dc87fec0cb72
RichardBray <richardbray89@gmail.com>
2023-07-26 17:08:16 +0100

    chore(lists): add POST controller for lists API
    - Import necessary dependencies and modules
    - Define the controller function `listsApiPostController` to handle POST requests to the `/api/lists` endpoint
    - Validate the received data using a Joi schema
    - If there is a validation error, log the error and send a 400 response with the error message
    - Use Prisma to query the database for a list that matches the received data
    - If no list is found, send a 404 response
    - If a list is found, send a 200 response
    - If there is an error during the process, log the error and send a 500 response
    
    chore(lists): add POST route for lists API
    
    - Add a POST route to the `/api/lists` endpoint in the lists API router
    - Enable CORS for the route
    
    TODO: Implement middleware for request signature verification
    
    - Add a TODO comment to remind to implement middleware for verifying the request signature
    - Define a secret key and retrieve the timestamp and signature from the request headers
    - Use crypto to create a HMAC with the secret key and the timestamp, and compare it with the received signature
    - If the signatures do not match, send a 401 response

--- src/server/components/lists/api/controllers.ts
+++ src/server/components/lists/api/controllers.ts
@@ -1,1 +1,45 @@
-export function listsApiPostController() {}
+import type { Request, Response } from "express";
+import Joi from "joi";
+import { prisma } from "server/models/db/prisma-client";
+import { logger } from "server/services/logger";
+
+export async function listsApiPostController(req: Request, res: Response) {
+  const receivedData = {
+    type: req.body.type as "funeralDirectors" | "translatorsInterpreters" | "lawyers",
+    country: req.body.country as string,
+  };
+
+  const { error } = receivedDataSchema.validate(receivedData);
+
+  if (error) {
+    logger.error(`listsApiPostController schema validation error: ${error.details[0].message}`);
+    res.status(400).send(error);
+    return;
+  }
+
+  try {
+    const result = await prisma.list.findFirst({
+      where: {
+        type: receivedData.type,
+        country: {
+          name: receivedData.country,
+        },
+      },
+    });
+
+    if (!result) {
+      res.sendStatus(404);
+      return;
+    }
+
+    res.sendStatus(200);
+  } catch (error) {
+    logger.error(`listsApiPostController error: ${error}`);
+    res.sendStatus(500);
+  }
+}
+
+const receivedDataSchema = Joi.object({
+  type: Joi.string().valid("funeralDirectors", "translatorsInterpreters", "lawyers").required(),
+  country: Joi.string().required(),
+});
--- src/server/components/lists/api/router.ts
+++ src/server/components/lists/api/router.ts
@@ -7,3 +7,17 @@ const apiRouter = express.Router();
 apiRouter.post("/api/lists", cors(), listsApiPostController);
 
 export default apiRouter;
+
+// TODO put this in middleware
+// const secretKey = "my-secret-key";
+
+// const timestamp = req.headers["timestamp"];
+
+// const signature = crypto.createHmac("sha512", secretKey)
+// .update(timestamp.toString())
+// .digest("hex");
+
+// if (signature !== req.headers["signature"]) {
+//   res.status(401).send("Unauthorized");
+//   return;
+// }
