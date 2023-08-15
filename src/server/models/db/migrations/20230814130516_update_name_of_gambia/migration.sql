1e2a59faa61348e2ff9fab09a6d5caddd815016f
RichardBray <richardbray89@gmail.com>
2023-08-15 14:08:55 +0100

    chore(migration.sql): update name of Gambia to The Gambia in jsonData field of ListItem table

--- src/server/models/db/migrations/20230814130516_update_name_of_gambia/migration.sql
+++ src/server/models/db/migrations/20230814130516_update_name_of_gambia/migration.sql
@@ -1,5 +1,16 @@
 -- Update name of Gambia to The Gambia
+-- In Country table
 
 UPDATE "Country"
 SET name = 'The Gambia'
 WHERE name = 'Gambia';
+
+-- In jsonData in ListItem table
+
+UPDATE "ListItem"
+SET "jsonData" = jsonb_set(
+  jsonb_set("jsonData", '{country}', '"The Gambia"'),
+  '{regions}',
+  '"The Gambia"'
+)
+WHERE "jsonData" ->> 'country' = 'Gambia';
