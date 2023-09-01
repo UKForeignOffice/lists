2dff5eff198bc3a2c9921b894a758493a0692349
jen <jen@cautionyourblast.com>
2023-09-01 16:24:14 +0100

    add run script

--- src/scheduler/workers/resendRequestedEditEmail/index.ts
+++ src/scheduler/workers/resendRequestedEditEmail/index.ts
@@ -9,7 +9,7 @@ import { RESEND_EDIT_REQUEST_EMAIL } from "server/config";
 export async function main() {
   logger.info(`resendRequestedEditEmail starting - RESEND_EDIT_REQUEST_EMAIL is ${RESEND_EDIT_REQUEST_EMAIL}`);
 
-  if (!RESEND_EDIT_REQUEST_EMAIL) {
+  if (RESEND_EDIT_REQUEST_EMAIL) {
     const listItems = await findListItemsWithRequestedEdit();
     const tasks = listItems.map(sendNewEditEmail);
     await Promise.allSettled(tasks);
---
+++ src/scheduler/workers/run.ts
@@ -0,0 +1,45 @@
+import { main as unpublishWeeklyTask } from "./unpublish/weekly";
+import { main as unpublishDayBeforeTask } from "./unpublish/dayBefore";
+import { main as unpublishDayTask } from "./unpublish/day";
+import { processAnnualReview as processListsBeforeAndDuringStart } from "./processListsBeforeAndDuringStart/main";
+import { main as resendRequestedEditEmail } from "./resendRequestedEditEmail";
+import { logger } from "scheduler/logger";
+import deleteItemsAfterAYear from "./unpublish/delete";
+
+const [args] = process.argv.slice(2);
+
+/**
+ * Use this to retry one task.
+ * `npm run task:resendRequestedEditEmail` or
+ * `node dist/scheduler/run resendRequestedEditEmail`
+ */
+async function run(taskName: string) {
+  logger.info(`Task requested was ${taskName}`);
+
+  const tasks = {
+    unpublishWeeklyTask,
+    unpublishDayTask,
+    unpublishDayBeforeTask,
+    processListsBeforeAndDuringStart,
+    resendRequestedEditEmail,
+    deleteItemsAfterAYear,
+  };
+  // @ts-ignore
+  const task = tasks[taskName];
+  if (!task) {
+    logger.error(`Task ${task} not found`);
+    process.exit(1);
+  }
+
+  return await task();
+}
+
+run(args)
+  .then(() => {
+    logger.info(`Task completed`);
+    process.exit(0);
+  })
+  .catch((promiseRejects) => {
+    logger.error(promiseRejects);
+    process.exit(1);
+  });
