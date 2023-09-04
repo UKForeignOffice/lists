63e98d662c5f33705921f5db661e73a2d3fa0f22
jen <jen@cautionyourblast.com>
2023-09-04 17:11:24 +0100

    better logs

--- src/scheduler/workers/resendRequestedEditEmail/sendNewEditEmail.ts
+++ src/scheduler/workers/resendRequestedEditEmail/sendNewEditEmail.ts
@@ -12,13 +12,17 @@ export async function sendNewEditEmail({
   listItem: ListItemWithAddressCountry;
   message: string;
 }) {
+  const { contactName, contactEmailAddress } = getListItemContactInformation(listItem);
+
   try {
     const changeLink = await initialiseFormRunnerSession({
       list: { type: listItem.type },
       listItem,
       message,
     });
-    const { contactName, contactEmailAddress } = getListItemContactInformation(listItem);
+    logger.info(
+      `attempting to send email to id: ${listItem.id} (${contactEmailAddress}) - with new link ${changeLink}`
+    );
     const typePlural = serviceName(listItem.type);
     const { result, error } = await sendEditDetailsEmail(
       contactName,
@@ -27,12 +31,13 @@ export async function sendNewEditEmail({
       message,
       changeLink
     );
+
     if (error) {
       throw error;
     }
     return result;
   } catch (e) {
-    logger.error(`Failed to send email for ${listItem.id}, ${e}`);
+    logger.error(`Failed to send email for ${listItem.id} (${contactEmailAddress}), ${e}`);
     throw e;
   }
 }
