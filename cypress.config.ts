17515425e30a054f5f7190923604c016feba0ca6
jen <jen@cautionyourblast.com>
2023-09-08 10:33:05 +0100

    video: true

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
 export default {
   chromeWebSecurity: false,
-  video: false, // change to true if debug required (.circleci.yml)
+  video: true, // change to true if debug required (.circleci.yml)
   projectId: "fo4z8h",
   failOnStatusCode: false,
   env: {
