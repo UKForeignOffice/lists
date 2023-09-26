6f54e9b2e2336b200dc800709e6ee882f90b1c6c
RichardBray <richardbray89@gmail.com>
2023-09-26 16:07:03 +0100

    test(a_csv_file_with_the_name_string_should_download.js): add functionality to set the download attribute of the export link to true and click on it to initiate the download of the CSV file with the specified name

--- cypress/support/step_definitions/dashboard/a_csv_file_with_the_name_string_should_download.js
+++ cypress/support/step_definitions/dashboard/a_csv_file_with_the_name_string_should_download.js
@@ -2,30 +2,17 @@ import { format } from "date-fns";
 import path from "path";
 
 Then("A csv file with the name {string} should download", (fileName) => {
-  // const currentDate = new Date();
-  // const formattedDate = format(currentDate, "yyyy-MM-dd");
-  // const downloadsFolder = Cypress.config('downloadsFolder');
+  const currentDate = new Date();
+  const formattedDate = format(currentDate, "yyyy-MM-dd");
+  const downloadsFolder = Cypress.config('downloadsFolder');
 
-  // const filename = path.join(downloadsFolder, `${fileName}-${formattedDate}.csv`)
-  // cy.readFile(filename, { timeout: 15000 }).should("exist");
-
-  // cy.reload(true, {timeout: 5000});
-
-
-  cy.intercept({ method: 'GET', url: '/dashboard/lists/634/csv' }, (req) => {
-    req.reply({
-      statusCode: 200,
-    });
-  }).as('responseRole');
-
-  cy.findAllByRole("link", {
+  const exportLink = cy.findAllByRole("link", {
     name: "Export list as CSV",
-  }).click(true);
-
-
-  cy.wait('@responseRole').then(({response}) => {
-    cy.log(response);
-    expect(response).to.include('attachment; filename=Eurasia-Lawyers-2023-09-25.csv')
-
   });
+
+  exportLink.invoke('attr', 'download', 'true');
+  exportLink.click();
+
+  const filename = path.join(downloadsFolder, `${fileName}-${formattedDate}.csv`)
+  cy.readFile(filename, { timeout: 5000 }).should("exist");
 });
