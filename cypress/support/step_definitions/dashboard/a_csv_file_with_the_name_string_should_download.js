c3866e131b2f2bda21306ed488eeb237620412a8
RichardBray <richardbray89@gmail.com>
2023-09-27 11:16:52 +0100

    chore: remove unnecessary click to export in smoke test

--- cypress/support/step_definitions/dashboard/a_csv_file_with_the_name_string_should_download.js
+++ cypress/support/step_definitions/dashboard/a_csv_file_with_the_name_string_should_download.js
@@ -6,13 +6,6 @@ Then("A csv file with the name {string} should download", (fileName) => {
   const formattedDate = format(currentDate, "yyyy-MM-dd");
   const downloadsFolder = Cypress.config('downloadsFolder');
 
-  const exportLink = cy.findAllByRole("link", {
-    name: "Export list as CSV",
-  });
-
-  exportLink.invoke('attr', 'download', 'true');
-  exportLink.click();
-
   const filename = path.join(downloadsFolder, `${fileName}-${formattedDate}.csv`)
   cy.readFile(filename, { timeout: 5000 }).should("exist");
 });
