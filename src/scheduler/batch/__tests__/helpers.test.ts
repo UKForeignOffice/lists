5d17681b940a5227b4f854eb3fdd2efce55f4c07
jen <jen@cautionyourblast.com>
2023-09-27 10:15:53 +0000

    tidying

--- src/scheduler/batch/__tests__/helpers.test.ts
+++ src/scheduler/batch/__tests__/helpers.test.ts
@@ -1,107 +1,5 @@
-import {
-  composeKeyDatesForDate,
-  DateContext,
-  getCurrentAnnualReviewData,
-  getDateContexts,
-  schedulerMilestoneDays,
-} from "../helpers";
+import { composeKeyDatesForDate } from "../helpers";
 import { startOfDay } from "date-fns";
-
-function testContext(actualUnpublishContext: DateContext[], expectedDateContexts: DateContext[], daysToAction: number) {
-  const actualContext = actualUnpublishContext.find((context) => context.eventMilestone === daysToAction);
-  const expectedContext = expectedDateContexts.find((context) => context.eventMilestone === daysToAction);
-  return { expectedContext, actualContext };
-}
-
-describe("Date Contexts", () => {
-  describe("annual review date contexts", () => {
-    const expectedDateContexts: DateContext[] = [
-      {
-        eventDate: new Date(2022, 10, 7, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.post.ONE_MONTH,
-      },
-      {
-        eventDate: new Date(2022, 10, 28, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.post.ONE_WEEK,
-      },
-      {
-        eventDate: new Date(2022, 11, 4, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.post.ONE_DAY,
-      },
-      {
-        eventDate: new Date(2022, 11, 5, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.both.START,
-      },
-    ];
-    const dateContexts = getDateContexts(new Date("2022-12-05"));
-    const actualAnnualReview = dateContexts.annualReview;
-
-    test.each([
-      schedulerMilestoneDays.post.ONE_MONTH,
-      schedulerMilestoneDays.post.ONE_WEEK,
-      schedulerMilestoneDays.post.ONE_DAY,
-      schedulerMilestoneDays.post.ONE_DAY,
-      schedulerMilestoneDays.both.START,
-    ])("milestone contexts", (milestoneDays) => {
-      const { expectedContext, actualContext } = testContext(actualAnnualReview, expectedDateContexts, milestoneDays);
-      expect(actualContext?.eventMilestone).toBe(expectedContext?.eventMilestone);
-      expect(actualContext?.eventDate.toString()).toBe(expectedContext?.eventDate.toString());
-    });
-  });
-
-  describe("unpublished date contexts", () => {
-    const expectedDateContexts: DateContext[] = [
-      {
-        eventDate: new Date(2022, 11, 12, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.provider.FIVE_WEEKS,
-      },
-      {
-        eventDate: new Date(2022, 11, 19, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.provider.FOUR_WEEKS,
-      },
-      {
-        eventDate: new Date(2022, 11, 26, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.provider.THREE_WEEKS,
-      },
-      {
-        eventDate: new Date(2023, 0, 2, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.provider.TWO_WEEKS,
-      },
-      {
-        eventDate: new Date(2023, 0, 9, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.both.ONE_WEEK,
-      },
-      {
-        eventDate: new Date(2023, 0, 15, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.both.ONE_DAY,
-      },
-      {
-        eventDate: new Date(2023, 0, 16, 0, 0, 0),
-        eventMilestone: schedulerMilestoneDays.both.UNPUBLISH,
-      },
-    ];
-    const dateContexts = getDateContexts(new Date("2022-12-05"));
-    const actualUnpublishContext = dateContexts.unpublish;
-
-    test.each([
-      schedulerMilestoneDays.provider.FIVE_WEEKS,
-      schedulerMilestoneDays.provider.FOUR_WEEKS,
-      schedulerMilestoneDays.provider.THREE_WEEKS,
-      schedulerMilestoneDays.provider.TWO_WEEKS,
-      schedulerMilestoneDays.both.ONE_WEEK,
-      schedulerMilestoneDays.both.ONE_DAY,
-      schedulerMilestoneDays.both.START,
-    ])("provider milestone contexts", (milestoneDays) => {
-      const { expectedContext, actualContext } = testContext(
-        actualUnpublishContext,
-        expectedDateContexts,
-        milestoneDays
-      );
-      expect(actualContext?.eventMilestone).toBe(expectedContext?.eventMilestone);
-      expect(actualContext?.eventDate.toString()).toBe(expectedContext?.eventDate.toString());
-    });
-  });
-});
 
 describe("Today Date", () => {
   describe("today is GMT timezone", () => {
@@ -122,14 +20,6 @@ describe("Today Date", () => {
     const actualDate = startOfDay(new Date());
     expect(actualDate.toISOString()).toBe(expectedDate);
   });
-});
-
-test("getCurrentAnnualReviewData returns the same dates as composeKeyDates", () => {
-  const contexts = getDateContexts(new Date("2022-05-01T00:00:00.000Z"));
-  const getCurrentAnnualReviewDataKeyDates = getCurrentAnnualReviewData([], contexts).keyDates;
-
-  const composedKeyDates = composeKeyDatesForDate(new Date("2022-05-01T00:00:00.000Z"));
-  expect(getCurrentAnnualReviewDataKeyDates).toEqual(composedKeyDates);
 });
 
 test("composeKeyDatesForDate is correct when today's date is 1st May 2022", () => {
