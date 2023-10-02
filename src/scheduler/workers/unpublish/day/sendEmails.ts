91c5275c535ebe4d722d5b5c33cbd91a2124dab0
jen <jen@cautionyourblast.com>
2023-10-02 15:02:23 +0100

    condition

--- src/scheduler/workers/unpublish/day/sendEmails.ts
+++ src/scheduler/workers/unpublish/day/sendEmails.ts
@@ -17,7 +17,7 @@ export async function main(list: List) {
     return;
   }
 
-  if (meta.daysUntilUnpublish > 0) {
+  if (meta.daysUntilUnpublish <= 0) {
     logger.info(
       `${meta.daysUntilUnpublish} does not match 0 day before unpublish, skipping sendEmailsToNonRespondents`
     );
