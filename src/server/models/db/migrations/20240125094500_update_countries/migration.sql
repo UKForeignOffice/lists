7ef928f5073b8ce828e1ecfe3245dc65e22ac582
Simon Moore <s.moore@kainos.com>
2024-01-24 15:06:58 +0000

    feat: Update country names

---
+++ src/server/models/db/migrations/20240125094500_update_countries/migration.sql
@@ -0,0 +1,19 @@
+-- UPDATE Micronesia
+UPDATE "Country"
+SET name = 'Federated States of Micronesia'
+WHERE name = 'Micronesia';
+
+-- UPDATE Saint Kitts and Nevis
+UPDATE "Country"
+SET name = 'St Kitts and Nevis'
+WHERE name = 'Saint Kitts and Nevis';
+
+-- UPDATE Saint Lucia
+UPDATE "Country"
+SET name = 'St Lucia'
+WHERE name = 'Saint Lucia';
+
+-- UPDATE Saint Vincent and the Grenadines
+UPDATE "Country"
+SET name = 'St Vincent'
+WHERE name = 'Saint Vincent and the Grenadines';
