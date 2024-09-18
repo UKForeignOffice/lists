b0ae926c854f1e191c6180732665b138efcebdce
Simon Moore <s.moore@kainos.com>
2024-09-18 11:18:45 +0100

    Change safelist on startup and hard code callback for testing

--- docker/apply/docker-entrypoint.sh
+++ docker/apply/docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find runner/dist/server/forms -type f -exec sed -i "s|http://lists|http://lists.$ENVIRONMENT.internal|g" {} +
+sed -i "s|lists|lists.$ENVIRONMENT.internal|g" runner/config/$NODE_ENV.json
 yarn runner start
--- src/server/components/formRunner/helpers.ts
+++ src/server/components/formRunner/helpers.ts
@@ -43,7 +43,7 @@ export function getNewSessionWebhookData({
   redirectUrl,
   userId,
 }: NewSessionWebhookDataInput): FormRunner.NewSessionData {
-  const callbackUrl = `http://lists:3000/ingest/${listType}/${listItemId}`;
+  const callbackUrl = `http://lists.integration.internal:3000/ingest/${listType}/${listItemId}`;
   const redirectPath = "/summary";
   const protocol = isLocalHost ? "http" : "https";
   const options = {
