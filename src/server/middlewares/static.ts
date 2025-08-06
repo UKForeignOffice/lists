a9571a325dd9ece8c9611ad483c281d2484c5a79
Simon Moore <s.moore@kainos.com>
2025-08-06 15:04:48 +0100

    Update to v5 of GOV UK Frontend. Bump app dependencies.

--- src/server/middlewares/static.ts
+++ src/server/middlewares/static.ts
@@ -3,29 +3,25 @@ import express, { Express } from "express";
 
 const ROOT = process.cwd();
 const NODE_MODULES = path.join(ROOT, "node_modules");
-const GOVUK_FRONTEND = path.join(NODE_MODULES, "govuk-frontend", "govuk");
 
 const publicFolderPath = path.join(__dirname, "client");
-const govUKAssetsFolderPath = path.join(GOVUK_FRONTEND, "assets");
-const govUKAllJSPath = path.join(GOVUK_FRONTEND, "all.js");
-const accessibleAutoCompletePath = path.join(
-  NODE_MODULES,
-  "accessible-autocomplete",
-  "dist"
-);
+const govUKAssetsFolderPath = path.join(NODE_MODULES, "govuk-frontend", "dist", "govuk", "assets");
+const govUKFrontendMinJSPath = path.join(NODE_MODULES, "govuk-frontend", "dist", "govuk", "govuk-frontend.min.js");
+const accessibleAutoCompletePath = path.join(NODE_MODULES, "accessible-autocomplete", "dist");
 const errorLogFile = path.join(ROOT, "error.log");
 
 export const configureStaticServer = (server: Express): void => {
   server.use("/assets", express.static(publicFolderPath));
   server.use("/assets", express.static(govUKAssetsFolderPath, {
-    setHeaders: (res, path) => {
-      const WEBFONT_EXTENSIONS = /\.(?:eot|ttf|woff|svg|woff2)$/i;
-      if (WEBFONT_EXTENSIONS.test(path)) {
-        res.header("cache-control", "public, max-age=604800, immutable");
-      }
-    }
-  }));
-  server.use("/assets/govuk-frontend/all.js", express.static(govUKAllJSPath));
+      setHeaders: (res, path) => {
+        const WEBFONT_EXTENSIONS = /\.(?:eot|ttf|woff|svg|woff2)$/i;
+        if (WEBFONT_EXTENSIONS.test(path)) {
+          res.header("cache-control", "public, max-age=604800, immutable");
+        }
+      },
+    })
+  );
+  server.use("/assets/govuk-frontend/govuk-frontend.min.js", express.static(govUKFrontendMinJSPath));
   server.use("/assets", express.static(accessibleAutoCompletePath));
   server.use("/error-log", express.static(errorLogFile));
 };
