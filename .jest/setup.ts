0cd5d3da4f4851a8a4ec5e70f852692b319d4fda
Simon Moore <s.moore@kainos.com>
2026-02-06 09:29:28 +0000

    Fix tests

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -14,7 +14,27 @@ if (typeof global.TextDecoder === "undefined") {
 }
 
 jest.mock("server/services/logger");
+
 jest.mock("server/services/redis");
+
+jest.mock("server/services/secrets-manager", () => ({
+  getSecretValue: jest.fn().mockResolvedValue("12345678"),
+}));
+
+jest.mock("@aws-sdk/client-location", () => ({
+  Location: jest.fn(() => ({
+    listPlaceIndexes: jest.fn().mockResolvedValue({ Entries: [] }),
+    createPlaceIndex: jest.fn().mockResolvedValue({}),
+    searchPlaceIndexForText: jest.fn().mockResolvedValue({ Results: [] }),
+  })),
+}));
+
+jest.mock("@aws-sdk/client-secrets-manager", () => ({
+  SecretsManagerClient: jest.fn().mockImplementation(() => ({
+    send: jest.fn().mockResolvedValue({ SecretString: "{}" }),
+  })),
+  GetSecretValueCommand: jest.fn(),
+}));
 
 jest.mock("crypto", () => {
   const crypto = jest.requireActual("crypto");
